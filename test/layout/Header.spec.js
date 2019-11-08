6638538ff8d528e8e478b2e909807c4b50fa44ec
Jacob See <jasee@redhat.com>
2019-11-08 14:02:10 -0800

    Fix tests that depend on Vuex store

--- test/layout/Header.spec.js
+++ test/layout/Header.spec.js
@@ -1,14 +1,31 @@
-import { mount } from '@vue/test-utils'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex';
 import Header from '@/components/layout/Header.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+var mockStore
+var wrapper
+
 describe('Header', () => {
+  beforeEach(() => {
+    mockStore = new Vuex.Store({
+      modules: {
+        auth: {
+          namespaced: false,
+          state: { user: { name: "name" }}
+        }
+      }
+    });
+  });
+
   test('is a Vue instance', () => {
-    const wrapper = mount(Header)
+    wrapper = shallowMount(Header, { store: mockStore, localVue });
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
   test("renders properly", () => {
-    const wrapper = mount(Header)
+    wrapper = shallowMount(Header, { store: mockStore, localVue });
     expect(wrapper.html()).toMatchSnapshot();
   });
 })
