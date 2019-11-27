3ed63504ccc8589b84db90db7a91d39d0dad7f1c
Jacob See <jacob.d.see@gmail.com>
2019-11-26 17:55:47 -0800

    Move wizard pages to own components & implement two-way data binding

---
+++ components/forms/BasicInfo.vue
@@ -0,0 +1,80 @@
+<template>
+  <form novalidate class="pf-c-form">
+    <div class="pf-c-form__group">
+      <label class="pf-c-form__label" for="customer-name">
+        <span class="pf-c-form__label-text">Customer</span>
+        <span class="pf-c-form__label-required" aria-hidden="true">
+          &#42;
+        </span>
+      </label>
+      <input
+        class="pf-c-form-control"
+        required
+        type="text"
+        v-model="customer"
+        @change="$emit('update:customer', customer)"
+      />
+      <p class="pf-c-form__helper-text" aria-live="polite">
+        The customer name to use for this residency
+      </p>
+    </div>
+    <div class="pf-c-form__group">
+      <label class="pf-c-form__label" for="customer-location">
+        <span class="pf-c-form__label-text">Location</span>
+        <span class="pf-c-form__label-required" aria-hidden="true">
+          &#42;
+        </span>
+      </label>
+      <input
+        class="pf-c-form-control"
+        required
+        type="email"
+        v-model="location"
+        @change="$emit('update:location', location)"
+      />
+      <p class="pf-c-form__helper-text" aria-live="polite">
+        Where the residency will be held
+      </p>
+    </div>
+    <div class="pf-c-form__group">
+      <label class="pf-c-form__label" for="simple-form-number">
+        <span class="pf-c-form__label-text">Dates</span>
+      </label>
+      <br />
+      <template>
+        <client-only>
+          <date-picker
+            v-model="dates"
+            input-class="pf-c-form-control"
+            range
+            @change="$emit('update:dates', dates)"
+          />
+        </client-only>
+      </template>
+      <p class="pf-c-form__helper-text" aria-live="polite">
+        When the residency will be held
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['customer', 'location', 'dates']
+}
+</script>
+
+<style>
+.pf-c-form-control {
+  width: 100%;
+}
+.mx-datepicker {
+  width: 100%;
+}
+.mx-input-wrapper {
+  width: 100%;
+}
+td.cell {
+  text-align: center;
+}
+</style>
--- pages/wizard.vue
+++ pages/wizard.vue
@@ -13,64 +13,7 @@
     </section>
     <Wizard :navigation="navItems" initialState="basic-information">
       <template slot="basic-information">
-        <form novalidate class="pf-c-form">
-          <div class="pf-c-form__group">
-            <label class="pf-c-form__label" for="customer-name">
-              <span class="pf-c-form__label-text">Customer</span>
-              <span class="pf-c-form__label-required" aria-hidden="true">
-                &#42;
-              </span>
-            </label>
-            <input
-              class="pf-c-form-control"
-              required
-              type="text"
-              id="customer-name"
-              name="customer-name"
-              v-model="customer"
-            />
-            <p class="pf-c-form__helper-text" aria-live="polite">
-              The customer name to use for this residency
-            </p>
-          </div>
-          <div class="pf-c-form__group">
-            <label class="pf-c-form__label" for="customer-location">
-              <span class="pf-c-form__label-text">Location</span>
-              <span class="pf-c-form__label-required" aria-hidden="true">
-                &#42;
-              </span>
-            </label>
-            <input
-              class="pf-c-form-control"
-              required
-              type="email"
-              id="customer-location"
-              name="customer-location"
-              v-model="location"
-            />
-            <p class="pf-c-form__helper-text" aria-live="polite">
-              Where the residency will be held
-            </p>
-          </div>
-          <div class="pf-c-form__group">
-            <label class="pf-c-form__label" for="simple-form-number">
-              <span class="pf-c-form__label-text">Dates</span>
-            </label>
-            <br />
-            <template>
-              <client-only>
-                <date-picker
-                  v-model="dates"
-                  input-class="pf-c-form-control"
-                  range
-                />
-              </client-only>
-            </template>
-            <p class="pf-c-form__helper-text" aria-live="polite">
-              When the residency will be held
-            </p>
-          </div>
-        </form>
+        <BasicInfo v-bind.sync="basic"></BasicInfo>
       </template>
       <template slot="point-of-contact">
         point of contact stuff
@@ -90,6 +33,7 @@
 
 <script>
 import Wizard from '~/components/patternfly/Wizard.vue'
+import BasicInfo from '~/components/forms/BasicInfo.vue'
 
 const navItems = [
   {
@@ -116,30 +60,18 @@ const navItems = [
 
 export default {
   components: {
-    Wizard
+    Wizard,
+    BasicInfo
   },
   data: () => {
     return {
       navItems,
-      dates: '',
-      customer: '',
-      location: ''
+      basic: {
+        dates: '',
+        customer: '',
+        location: ''
+      }
     }
   }
 }
 </script>
-
-<style>
-.pf-c-form-control {
-  width: 100%;
-}
-.mx-datepicker {
-  width: 100%;
-}
-.mx-input-wrapper {
-  width: 100%;
-}
-td.cell {
-  text-align: center;
-}
-</style>
