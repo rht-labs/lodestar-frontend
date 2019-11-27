be22b3dcc4a907dbd73d84b0082faac879580de8
Jacob See <jacob.d.see@gmail.com>
2019-11-26 23:53:55 -0800

    The oauth callback page looks bad and I might as well make it nicer.

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,16 +1,19 @@
 <template>
-  <div
-    class="container d-flex align-items-center justify-content-center flex-column"
+  <main
+    id="main-content-page-default-nav"
+    role="main"
+    class="pf-c-page__main"
+    tabindex="-1"
   >
-    Logging in...
-  </div>
+    <div class="pf-c-page__main-section">
+      <div class="container">
+        <div class="full-height">
+          <div class="loader"></div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
-
-<style scoped>
-.container {
-  min-height: 70vh;
-}
-</style>
 
 <script>
 export default {
@@ -18,3 +21,41 @@ export default {
   layout: 'empty'
 }
 </script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: calc(
+    100vh - 76px - var(--pf-c-page__main-section--PaddingTop) -
+      var(--pf-c-page__main-section--PaddingBottom)
+  );
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.full-height {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+.loader {
+  border: 16px solid #ffe6e6; /* Light grey */
+  border-top: 16px solid #ee0000; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
