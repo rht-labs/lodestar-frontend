6f341b93ce85116cb29541d9963e01c217c415da
Lucas Allmon <lallmon@redhat.com>
2020-01-16 13:59:18 +0000

    chore(app): app render snapshot too big

--- src/App.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'enzyme';
-import App from './App';
-
-it('renders without crashing', () => {
-  const app = render(<App />);
-  expect(app).toMatchSnapshot();
-});
