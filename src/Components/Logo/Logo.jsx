1599f169f8c1fb651ee423d52b77eeaac04098aa
Lucas Allmon <lallmon@redhat.com>
2020-01-21 16:15:11 +0000

    feature(api): config/post

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => (
   <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
     <img
-      style={{ width: "70%" }}
+      style={{ width: "50%" }}
       src="https://www.redhat.com/cms/managed-files/RHOIL_LogoBadge_RGB_Default.svg"
       alt="Open Innovation Labs Logo"
     />
