96086145ac1caf299d1b6e5078cc66cee346ef2b
Lucas Allmon <lallmon@redhat.com>
2020-01-21 16:15:11 +0000

    feature(api): config/post

--- src/Steps/02_PointOfContact.spec.js
+++ src/Steps/02_PointOfContact.spec.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render } from "enzyme";
 import PointOfContact from "./02_PointOfContact";
+import defaultValues from "../initialState";
 
 test("Basic component renders correctly", () => {
-  const component = render(<PointOfContact />);
+  const component = render(<PointOfContact values={defaultValues} />);
   expect(component).toMatchSnapshot();
 });
