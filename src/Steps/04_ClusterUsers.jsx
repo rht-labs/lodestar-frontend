aaf2d737f9b90f9e76c501f039d4ee7c952beddb
Philip Double <philip.double@gmail.com>
2020-02-11 13:46:04 -0500

    103-add-users removed the roster title header from the page.

--- src/Steps/04_ClusterUsers.jsx
+++ src/Steps/04_ClusterUsers.jsx
@@ -57,86 +57,81 @@ const ClusterUsers = ({ options, values, onChange }) => {
             </div>
           </div>
         ) : (
-          <div 
-            style={{ fontSize: "2rem" }}
-          >
-            Roster
-            <Form isHorizontal>
-              <ul>
-                <li>
+          <Form isHorizontal>
+            <ul>
+              <li>
+                <InputGroup>
+                  <Text style={listHeader}>Last Name</Text>
+                  <Text style={listHeader}>First Name</Text>
+                  <Text style={listHeader}>email</Text>
+                  <Text style={listHeader}>Role</Text>
+                  <Text style={buttonHeader}>Del</Text>
+                </InputGroup>
+              </li>
+              {values.engagement_users.map((value, index) => {
+                return <li key={index}>
                   <InputGroup>
-                    <Text style={listHeader}>Last Name</Text>
-                    <Text style={listHeader}>First Name</Text>
-                    <Text style={listHeader}>email</Text>
-                    <Text style={listHeader}>Role</Text>
-                    <Text style={buttonHeader}>Del</Text>
-                  </InputGroup>
+                    <TextInput
+                      aria-label="Last Name"
+                      name="last-name"
+                      onChange={e => {
+                        values.engagement_users[index].last_name = e;
+                        onChange({ type: "user", payload: values.engagement_users });
+                      }}
+                      placeholder="Last Name"
+                      type="text"
+                      value={value.last_name || ''}
+                    />
+                    <TextInput
+                      aria-label="First Name"
+                      name="first-name"
+                      helper="first name"
+                      onChange={e => {
+                        values.engagement_users[index].first_name = e;
+                        onChange({ type: "user", payload: values.engagement_users });
+                      }}
+                      placeholder="First Name"
+                      type="text"
+                      value={value.first_name || ''}
+                    />
+                    <TextInput
+                      aria-label="email"
+                      name="email"
+                      onChange={e => {
+                        values.engagement_users[index].email = e;
+                        onChange({ type: "user", payload: values.engagement_users });
+                      }}
+                      placeholder="Email Address"
+                      type="email"
+                      value={value.email || ''}
+                    />
+                    <FormSelect
+                      style={selectStyle}
+                      name="role"
+                      aria-label="User Role"
+                      value={value.role || ''}
+                      onChange={e => {
+                        values.engagement_users[index].role = e;
+                        onChange({ type: "user", payload: values.engagement_users });
+                      }}
+                    >
+                      {options["user-management"].rbac.roles.map((option, index) => (
+                        <FormSelectOption
+                          isDisabled={option.disabled}
+                          key={index}
+                          value={option.value}
+                          label={option.label}
+                        />
+                      ))}
+                    </FormSelect>
+                    <Button onClick={removeUser} value={index} variant="danger" isInline>
+                      <ErrorCircleOIcon />
+                    </Button>
+                  </InputGroup> 
                 </li>
-                {values.engagement_users.map((value, index) => {
-                  return <li key={index}>
-                    <InputGroup>
-                      <TextInput
-                        aria-label="Last Name"
-                        name="last-name"
-                        onChange={e => {
-                          values.engagement_users[index].last_name = e;
-                          onChange({ type: "user", payload: values.engagement_users });
-                        }}
-                        placeholder="Last Name"
-                        type="text"
-                        value={value.last_name || ''}
-                      />
-                      <TextInput
-                        aria-label="First Name"
-                        name="first-name"
-                        helper="first name"
-                        onChange={e => {
-                          values.engagement_users[index].first_name = e;
-                          onChange({ type: "user", payload: values.engagement_users });
-                        }}
-                        placeholder="First Name"
-                        type="text"
-                        value={value.first_name || ''}
-                      />
-                      <TextInput
-                        aria-label="email"
-                        name="email"
-                        onChange={e => {
-                          values.engagement_users[index].email = e;
-                          onChange({ type: "user", payload: values.engagement_users });
-                        }}
-                        placeholder="Email Address"
-                        type="email"
-                        value={value.email || ''}
-                      />
-                      <FormSelect
-                        style={selectStyle}
-                        name="role"
-                        aria-label="User Role"
-                        value={value.role || ''}
-                        onChange={e => {
-                          values.engagement_users[index].role = e;
-                          onChange({ type: "user", payload: values.engagement_users });
-                        }}
-                      >
-                        {options["user-management"].rbac.roles.map((option, index) => (
-                          <FormSelectOption
-                            isDisabled={option.disabled}
-                            key={index}
-                            value={option.value}
-                            label={option.label}
-                          />
-                        ))}
-                      </FormSelect>
-                      <Button onClick={removeUser} value={index} variant="danger" isInline>
-                        <ErrorCircleOIcon />
-                      </Button>
-                    </InputGroup> 
-                  </li>
-                })}
-              </ul>
-            </Form>
-          </div>
+              })}
+            </ul>
+          </Form>
         )}
         <Button onClick={addUser} variant="link" icon={<PlusCircleIcon />}>
           Add User
