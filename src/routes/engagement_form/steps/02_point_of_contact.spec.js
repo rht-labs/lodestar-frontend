a30d74df4f008745ebfe6edba7d2db14ce0ff718
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-02-25 16:43:42 -0700

    moved local engagement_form state to that route's subfolder

--- src/routes/engagement_form/steps/02_point_of_contact.spec.js
+++ src/routes/engagement_form/steps/02_point_of_contact.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "enzyme";
 import PointOfContact from "./02_point_of_contact";
-import defaultValues from "../../../initial_state";
+import defaultValues from "../initial_state";
 
 test("Basic component renders correctly", () => {
   const component = render(<PointOfContact values={defaultValues} />);
