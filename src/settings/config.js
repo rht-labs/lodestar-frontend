ea2ea7c8e5fb794a2a4796e8ca8b6372d957f321
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-02-25 11:45:53 -0700

    updated env example and migrated backenduri to appsettings

--- src/settings/config.js
+++ src/settings/config.js
@@ -2,5 +2,6 @@ export const AppSettings = {
     baseUrl: process.env.REACT_APP_BASE_URL,
     clientId: process.env.REACT_APP_CLIENT_ID,
     redirectUri: process.env.REACT_APP_REDIRECT_URI,
-    authBaseUrl: process.env.REACT_APP_AUTH_BASE_URL
+    authBaseUrl: process.env.REACT_APP_AUTH_BASE_URL,
+    backendUrl: process.env.REACT_APP_BACKEND_URI
 }
