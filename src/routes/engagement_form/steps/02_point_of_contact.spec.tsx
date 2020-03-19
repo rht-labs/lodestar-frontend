36ae661caab1a3928ebec6764c40bd8098c9b9f8
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-03-19 11:51:47 -0600

    fixed tests

--- src/routes/engagement_form/steps/02_point_of_contact.spec.tsx
+++ src/routes/engagement_form/steps/02_point_of_contact.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'enzyme';
 import { PointOfContact } from './02_point_of_contact';
-import defaultValues from '../initial_state';
+import { initialState } from '../initial_state';
 
 test('Basic component renders correctly', () => {
-  const component = render(<PointOfContact values={defaultValues} />);
+  const component = render(<PointOfContact values={initialState} />);
   expect(component).toMatchSnapshot();
 });
