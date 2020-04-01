05352c4f0ff269df053892c472a882487b057c7b
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-04-01 10:34:59 -0600

    Bugfix/create engagement authenticated (#66)
    * updated endpoints
    
    * fixed auth issue when creating a config

--- src/repositories/engagement/faked_engagement_repository.ts
+++ src/repositories/engagement/faked_engagement_repository.ts
@@ -1,11 +1,28 @@
 import { EngagementRepository } from './engagement_repository';
 import { Engagement } from '../../models/engagement';
 import faker from 'faker';
+import Axios, { AxiosInstance } from 'axios';
 
 export class FakedEngagementRepository extends EngagementRepository {
+  constructor({
+    axios = Axios.create(),
+    baseUrl,
+  }: {
+    axios: AxiosInstance;
+    baseUrl: string;
+  }) {
+    super();
+    this.axios = axios;
+    this.baseUrl = baseUrl;
+  }
+  baseUrl: string;
+  axios: AxiosInstance;
   async fetchEngagements(): Promise<Engagement[]> {
     return new Array(8)
       .fill(null)
       .map(() => new Engagement({ name: faker.company.companyName() }));
+  }
+  async createEngagement(data: any): Promise<void> {
+    this.axios.post(`${this.baseUrl}/engagements`, data);
   }
 }
