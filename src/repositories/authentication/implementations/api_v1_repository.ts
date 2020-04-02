b69acb892e8ca922f8af00b739c5b8be462acc68
Jacob See <jacob.d.see@gmail.com>
2020-04-02 15:37:06 -0700

    add getTime() call

--- src/repositories/authentication/implementations/api_v1_repository.ts
+++ src/repositories/authentication/implementations/api_v1_repository.ts
@@ -65,7 +65,6 @@ export class ApiV1AuthenticationRepository implements AuthenticationRepository {
    * @returns {Promise<boolean>}
    */
   isLoggedIn = (): Promise<boolean> => {
-    console.log('are we logged in?')
     return new Promise((resolve, reject) => {
       try {
         const token = this.getToken();
@@ -76,21 +75,17 @@ export class ApiV1AuthenticationRepository implements AuthenticationRepository {
           ? token.refreshTokenExpiry.getTime() > new Date(Date.now()).getTime()
           : false;
         if (isAccessTokenValid) {
-          console.log('yes')
           // Access token is valid! Proceed as normal
           resolve(true);
         } else if (!isAccessTokenValid && isRefreshTokenValid) {
-          console.log('refreshing')
           // Fetch new access token using refresh token
           this.fetchToken(
             token?.refreshToken ? token.refreshToken : '',
             'refresh_token'
           ).then(token => {
-            console.log('refreshed!')
-            resolve(token && token.accessTokenExpiry > new Date(Date.now()));
+            resolve(token && token.accessTokenExpiry.getTime() > new Date(Date.now()).getTime());
           });
         } else {
-          console.log('no!')
           // Nothing is valid!
           resolve(false);
         }
