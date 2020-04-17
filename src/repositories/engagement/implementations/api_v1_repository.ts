657faaaa248c89dc84c63717ac63e032320e9970
Philip Double <philip.double@gmail.com>
2020-04-17 18:39:52 -0400

    Resolved refresh error.

--- src/repositories/engagement/implementations/api_v1_repository.ts
+++ src/repositories/engagement/implementations/api_v1_repository.ts
@@ -1,24 +1,26 @@
 import { EngagementRepository } from '../engagement_repository';
 import { Engagement } from '../../../models/engagement';
-import Axios, { AxiosInstance } from 'axios';
+import { AxiosInstance } from 'axios';
 
 export class ApiV1EngagementRepository extends EngagementRepository {
   constructor({
-    axios = Axios.create(),
+    axios,
     baseUrl,
   }: {
     axios?: AxiosInstance;
     baseUrl?: string;
   }) {
     super();
+    if (!axios) {
+      throw new Error('axios is required')
+    }
     this.axios = axios;
     this.baseUrl = baseUrl;
   }
   baseUrl?: string;
   axios?: AxiosInstance;
   async fetchEngagements(): Promise<Engagement[]> {
-      const {data: engagementsData} = await this.axios.get(
-      'https://omp-backend-domp2.apps.s11.core.rht-labs.com/engagements');
+      const {data: engagementsData} = await this.axios.get(`${this.baseUrl}/engagements`);
       return engagementsData.map((engagementMap) => new Engagement(engagementMap as Engagement));
   }
   async createEngagement(data: any): Promise<void> {
