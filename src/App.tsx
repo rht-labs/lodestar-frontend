4d7b9bf7d9b5cd841dd3d52f249469478db89f7a
Philip Double <philip.double@gmail.com>
2020-04-17 20:25:52 -0400

    Moved router to it's own file in the routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,14 @@ import {
 
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
 } from 'react-router-dom';
 
-import { FeatureRequest } from './components/feature_request';
-import { PrivateRoute } from './components/authentication/private_route';
-import { CallbackHandler } from './components/authentication/callback_handler';
-import { EngagementPane } from './routes/engagement_pane';
-import { Admin } from './routes/admin';
-import { Dashboard } from './routes/dashboard';
-import { EngagementForm } from './routes/engagement_form';
 import { SessionProvider } from './context/session_context';
 import { ConfigProvider } from './context/config_context';
-import { EngagementFormProvider } from './context/engagement_form_context';
-
 import { EngagementProvider } from './context/engagement_context';
 import { ErrorBoundary } from './components/error_boundary';
 import { OMPHeader } from './components/omp_header';
+import { OMPRouter } from './routes/router'
 
 export const App = () => {
   return (
@@ -50,51 +39,7 @@ const Providers = ({ children }: { children: React.ReactChild }) => {
 
 const MainTemplateRoutes = () => {
   return (
-    <Switch>
-      <PrivateRoute
-        exact
-        path="/"
-        component={() => {
-          return (
-            <Dashboard />
-          );
-        }}
-      />
-      <PrivateRoute
-        exact
-        path="/engagements"
-        component={() => {
-          return (
-            <EngagementFormProvider>
-              <EngagementPane />
-            </EngagementFormProvider>
-          );
-        }}
-      />
-      <PrivateRoute
-        exact
-        path="/wizard"
-        component={() => {
-          return (
-            <EngagementFormProvider>
-              <EngagementForm />
-            </EngagementFormProvider>
-          );
-        }}
-      />
-      <PrivateRoute
-        exact
-        path="/admin"
-        component={() => {
-          return (
-            <Admin />
-          );
-        }}
-      />
-      <Route path="/feature-request" component={FeatureRequest} />
-      <PrivateRoute path="/private" component={() => <Redirect to="/" />} />
-      <Route path="/auth_callback" component={CallbackHandler} />
-    </Switch>
+    <OMPRouter/>
   );
 };
 
