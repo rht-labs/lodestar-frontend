41f5e6915711765c0f580e4a0ea481f644b598ae
Philip Double <admin@cognizen.com>
2020-04-23 18:02:12 -0400

    Added ability to disable launch button and provide disabled message (#88)

--- src/components/omp_engagement_button_pane.tsx
+++ src/components/omp_engagement_button_pane.tsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
-import { Button } from '@patternfly/react-core';
+import { Button, Tooltip } from '@patternfly/react-core';
 import { EngagementFormContext } from '../context/engagement_form_context';
 import { EngagementContext } from '../context/engagement_context';
+import { ConfigContext } from '../context/config_context';
 import { slugProperties } from '../utilities/slug_properties';
 
 function _OMPEngagementButtonPane() {
   const engagementContext = useContext(EngagementContext);
   const engagementFormContext = useContext(EngagementFormContext);
-  
+  const configContext = useContext(ConfigContext);
+
   const buttonPane: React.CSSProperties = {
     position: 'absolute',
     right: 10,
@@ -35,10 +37,22 @@ function _OMPEngagementButtonPane() {
     engagementContext.saveEngagement(engagementFormContext.state);
   }
 
-  return (
+  //TODO: Set this to config value.
+  const isLaunchDisabled = configContext.appConfig?.disableLaunch;
+  const launchTooltip: string = isLaunchDisabled ? 'Launching new Engagements is currently unavailable. Please try again later.': 'Launch cluster for this engagement.'
+
+  return (  
     <div style={ buttonPane } >
       <Button onClick={saveCluster} style={ buttonDisplay } > Save </Button>
-      <Button isDisabled={true} onClick={launchCluster} style={ buttonDisplay } > Launch </Button>
+      <Tooltip
+        content={
+          <div>{launchTooltip}</div>
+        }
+      >
+        <span>
+        <Button isDisabled={isLaunchDisabled} onClick={launchCluster} style={ buttonDisplay } > Launch </Button>
+        </span>
+      </Tooltip>
     </div>
   );
 }
