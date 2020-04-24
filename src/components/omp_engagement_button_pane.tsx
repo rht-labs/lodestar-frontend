9e0f145468b49631a19f1c6921f18e8baedb1078
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-04-24 10:50:51 -0600

    refactored launch button

--- src/components/omp_engagement_button_pane.tsx
+++ src/components/omp_engagement_button_pane.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Button, Tooltip } from '@patternfly/react-core';
 import { EngagementFormContext } from '../context/engagement_form_context';
 import { EngagementContext } from '../context/engagement_context';
@@ -16,51 +16,56 @@ function _OMPEngagementButtonPane() {
     backgroundColor: 'rgba(255,255,255, 0.6)',
     padding: 5,
     borderRadius: 4,
-  }
+  };
 
   const buttonDisplay: React.CSSProperties = {
     margin: 5,
-  }
+  };
 
   const launchCluster = () => {
     engagementContext.launchEngagement(engagementFormContext.state);
-  }
+  };
 
   const saveCluster = () => {
     engagementContext.saveEngagement(engagementFormContext.state);
-  }
+  };
 
-  const [isLaunchDisabled, setIsLaunchDisabled] = useState<boolean>(
-    false
-  );
-
-  const [launchTooltip, setLaunchTooltip] = useState<string>(
-    "Launch the cluster for this engagement."
-  )
-  
-  useEffect(() => {
-    if(configContext.appConfig?.disableLaunch){
-      setIsLaunchDisabled(true);
-      setLaunchTooltip("Launching new Engagement clusters is currently unavailable. Please try again later.");
-    } else if( engagementContext.activeEngagement?.launch !== undefined ){
-      setIsLaunchDisabled(true);
-      setLaunchTooltip("The cluster for this engagement has been launched.")
+  const isLaunchDisabled = () => {
+    if (configContext.appConfig?.disableLaunch) {
+      return true;
+    } else if (engagementContext.activeEngagement?.launch !== undefined) {
+      return true;
     } else {
-      setIsLaunchDisabled(false);
-      setLaunchTooltip("Launch the cluster for this engagement.")
+      return false;
     }
-  }, [configContext.appConfig, engagementContext.activeEngagement]);
+  };
 
-  return (  
-    <div style={ buttonPane } >
-      <Button onClick={saveCluster} style={ buttonDisplay } > Save </Button>
-      <Tooltip
-        content={
-          <div>{launchTooltip}</div>
-        }
-      >
+  const getTooltipText = () => {
+    if (configContext.appConfig?.disableLaunch) {
+      return 'Launching new Engagement clusters is currently unavailable. Please try again later.';
+    } else if (engagementContext.activeEngagement?.launch !== undefined) {
+      return 'The cluster for this engagement has been launched.';
+    } else {
+      return 'Launch the cluster for this engagement.';
+    }
+  };
+
+  return (
+    <div style={buttonPane}>
+      <Button onClick={saveCluster} style={buttonDisplay}>
+        {' '}
+        Save{' '}
+      </Button>
+      <Tooltip content={<div>{getTooltipText()}</div>}>
         <span>
-        <Button isDisabled={isLaunchDisabled} onClick={launchCluster} style={ buttonDisplay } > Launch </Button>
+          <Button
+            isDisabled={isLaunchDisabled()}
+            onClick={launchCluster}
+            style={buttonDisplay}
+          >
+            {' '}
+            Launch{' '}
+          </Button>
         </span>
       </Tooltip>
     </div>
