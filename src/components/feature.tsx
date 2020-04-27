53761bafce8b9afafe2752ae32408b9acec23705
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-04-27 16:51:13 -0600

    added docstring explaining logic in Feature component

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -5,8 +5,17 @@ import { AppFeature } from '../common/app_features';
 export interface FeatureProps {
   children: React.ReactNode;
   inactiveComponent?: React.ComponentType<any>;
-  name: AppFeature;
+  name?: AppFeature;
 }
+
+/**
+ * 
+ * By default, Feature shows the feature-protected component if the feature name provided
+ * is included in the current user's roles, or if there is no feature name provided at all.
+ * 
+ * The reason for showing the feature if there is no feature name is to allow developers to
+ * proactively wrap product features in a Feature component and add the flag later.
+ */
 export const Feature = ({
   inactiveComponent: Component,
   name,
