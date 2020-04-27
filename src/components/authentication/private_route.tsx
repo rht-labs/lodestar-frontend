0c6310d694ad814ee5f3e16ff1d1480b01a0a527
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-04-27 15:16:51 -0600

    refactored routes

--- src/components/authentication/private_route.tsx
+++ src/components/authentication/private_route.tsx
@@ -1,23 +1,15 @@
 import React, { useContext } from 'react';
-import { Redirect, Route, RouteProps } from 'react-router-dom'
+import { Redirect, Route, RouteProps } from 'react-router-dom';
 import { SendToSSO } from './send_to_sso';
 import { SessionContext } from '../../context/session_context';
 
-interface PrivateRouteProps extends RouteProps {
-  path: string;
-  component: (props: any) => JSX.Element;
-}
-
-export const PrivateRoute = ({
-  component: Component,
-  ...rest
-}: PrivateRouteProps) => {
+export const PrivateRoute = (props: RouteProps) => {
   const sessionContext = useContext(SessionContext);
 
   if (sessionContext.authState === 'authenticated') {
-    return <Route {...rest} render={props => <Component {...props} />}/>;
+    return <Route {...props} />;
   } else if (sessionContext.authState === 'unauthorized') {
-    return <Redirect to="/unauthorized" />
+    return <Redirect to="/unauthorized" />;
   } else if (sessionContext.authState === 'unauthenticated') {
     return <SendToSSO />;
   } else {
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { FeatureRequest } from '../components/feature_request';
 import { PrivateRoute } from '../components/authentication/private_route';
@@ -13,46 +9,35 @@ import { Admin } from './admin';
 import { Dashboard } from './dashboard';
 import { EngagementFormProvider } from '../context/engagement_form_context';
 import { UnauthorizedPage } from './unauthorized';
-import LogoutPage from './logout'
+import LogoutPage from './logout';
+import { Feature } from '../components/feature';
 
 function _OMPRouter() {
-
   return (
     <Switch>
-      <PrivateRoute
-        exact
-        path="/"
-        component={() => {
-          return (
-            <Dashboard />
-          );
-        }}
-      />
-      <PrivateRoute
-        exact
-        path="/engagements"
-        component={() => {
-          return (
-            <EngagementFormProvider>
-              <EngagementPane />
-            </EngagementFormProvider>
-          );
-        }}
-      />
-      <PrivateRoute
-        exact
-        path="/admin"
-        component={() => {
-          return (
-            <Admin />
-          );
-        }}
-      />
-      <PrivateRoute path="/private" component={() => <Redirect to="/" />} />
-      <Route path="/logout" component={() => <LogoutPage />} />
       <Route path="/feature-request" component={FeatureRequest} />
       <Route path="/auth_callback" component={CallbackHandler} />
       <Route path="/unauthorized" component={UnauthorizedPage} />
+      // all other routes should be considered private
+      <PrivateRoute path="/">
+        <Feature name="manage_projects" inactiveComponent={UnauthorizedPage}>
+          // if a user is not authorized, show the unauthorized page
+          <Switch>
+            // else, show an authorized route
+            <Redirect exact from="/" to="/dashboard" />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/engagements">
+              <EngagementFormProvider>
+                <EngagementPane />
+              </EngagementFormProvider>
+            </PrivateRoute>
+            <PrivateRoute exact path="/admin">
+              <Admin />
+            </PrivateRoute>
+            <Route path="/logout" component={() => <LogoutPage />} />
+          </Switch>
+        </Feature>
+      </PrivateRoute>
     </Switch>
   );
 }
