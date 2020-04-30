6dfe82121ab63eb691043b68d887c10826f537ea
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-04-30 16:12:51 -0600

    added another example test

--- src/context/engagement_context/engagement_hook.spec.tsx
+++ src/context/engagement_context/engagement_hook.spec.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-
 import { renderHook, act } from '@testing-library/react-hooks';
 import { FakedEngagementService } from '../../services/engagement_service/implementations/faked_engagement_service';
 import { useEngagements } from './engagement_hook';
+import { getInitialState } from './engagement_form_reducer';
 
-test('Fetch Engagements', async () => {
-  const { result, waitForNextUpdate } = renderHook(() =>
-    useEngagements({ engagementService: new FakedEngagementService() })
-  );
-
-  expect(result.current).toBeTruthy();
-  expect(Array.isArray(result.current.engagements)).toBeTruthy();
-  await act(async () => {
-    jest.useFakeTimers();
-    result.current.fetchEngagements();
-    jest.runAllTimers();
-    await waitForNextUpdate();
+describe('Engagement Context', () => {
+  test('Fetch Engagements', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useEngagements({ engagementService: new FakedEngagementService() })
+    );
+    expect(result.current).toBeTruthy();
+    expect(Array.isArray(result.current.engagements)).toBeTruthy();
+    await act(async () => {
+      result.current.fetchEngagements();
+      await waitForNextUpdate();
+    });
+    expect(result.current.engagements.length).toBeGreaterThan(0);
   });
 
-  expect(result.current.engagements.length).toBeGreaterThan(0);
+  test('Can Modify Engagement Form', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useEngagements({ engagementService: new FakedEngagementService() })
+    );
+    expect(result.current.engagementFormState).toEqual(getInitialState());
+    act(async () => {
+      result.current.updateEngagementFormField('customer_name', 'spencer');
+      await waitForNextUpdate();
+    });
+    expect(result.current.engagementFormState.customer_name).toEqual('spencer');
+  });
 });
