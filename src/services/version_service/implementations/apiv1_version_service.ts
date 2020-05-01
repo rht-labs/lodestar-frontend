745e0a3e8bf2932dab829166d0ac17da6eb2fd66
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-05-01 12:47:59 -0600

    fix for version api

--- src/services/version_service/implementations/apiv1_version_service.ts
+++ src/services/version_service/implementations/apiv1_version_service.ts
@@ -5,10 +5,12 @@ import Axios, { AxiosInstance } from 'axios';
 export class Apiv1VersionService extends VersionService {
   constructor(baseURL: string, onBeforeRequest, onAfterRequest, onFailure) {
     super();
-    this.axios = Axios.create({ baseURL });
+    this.axios = Axios.create();
+    this.baseUrl = baseURL;
     this.axios.interceptors.request.use(onBeforeRequest);
     this.axios.interceptors.response.use(onAfterRequest, onFailure);
   }
+  baseUrl: string;
   axios?: AxiosInstance;
 
   private async fetchManifest(): Promise<object> {
@@ -27,7 +29,7 @@ export class Apiv1VersionService extends VersionService {
   }
 
   async fetchVersion(): Promise<Version> {
-    const { data } = await this.axios.get(`/api/v1/version`);
+    const { data } = await this.axios.get(`${this.baseUrl}/api/v1/version`);
     const fe = await this.fetchManifest();
     return Version.fromMap({ ...data, fe });
   }
