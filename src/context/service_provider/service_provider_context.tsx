e28d5353dc1fb44d3a8dee8abee954d45d5bba8a
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-05-01 12:09:18 -0600

    dependency injection 'roughly' working

--- src/context/service_provider/service_provider_context.tsx
+++ src/context/service_provider/service_provider_context.tsx
@@ -3,6 +3,7 @@ import { EngagementService } from '../../services/engagement_service/engagement_
 import { AuthService } from '../../services/authentication_service/authentication_service';
 import { VersionService } from '../../services/version_service/version_service';
 import { Apiv1VersionService } from '../../services/version_service/implementations/apiv1_version_service';
+import { FakedVersionService } from '../../services/version_service/implementations/faked_version_service';
 import { Apiv1EngagementService } from '../../services/engagement_service/implementations/apiv1_engagement_service';
 import { Apiv1AuthService } from '../../services/authentication_service/implementations/apiv1_auth_service';
 import { FakedAuthService } from '../../services/authentication_service/implementations/faked_auth_service';
@@ -19,13 +20,13 @@ interface ServiceProvider {
 const ProductionServiceProviders = (config: Config) => ({
   engagementService: new Apiv1EngagementService(config.backendUrl),
   authenticationService: new Apiv1AuthService(config),
-  versionService: new Apiv1VersionService(config.baseUrl),
+  versionService: new Apiv1VersionService(config.backendUrl),
 });
 
 const FakedServiceProviders = (config: Config) => ({
   engagementService: new FakedEngagementService(),
   authenticationService: new FakedAuthService(),
-  versionService: new Apiv1VersionService(config.baseUrl),
+  versionService: new FakedVersionService(),
 });
 
 export const ServiceProviderContext = React.createContext<ServiceProvider>({
@@ -38,7 +39,11 @@ export const ServiceProvider = ({ children }: { children: any }) => {
   const configContext = useContext(ConfigContext);
   return (
     <ServiceProviderContext.Provider
-      value={ProductionServiceProviders(configContext.appConfig)}
+      value={
+        process.env.REACT_APP_USE_FAKED
+          ? FakedServiceProviders(configContext.appConfig)
+          : ProductionServiceProviders(configContext.appConfig)
+      }
     >
       {children}
     </ServiceProviderContext.Provider>
