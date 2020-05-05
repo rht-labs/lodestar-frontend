93065b4859010c774acd9dfaea9c549976ae546a
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-05-05 10:57:02 -0600

    added additional tests

--- src/common/validator.spec.ts
+++ src/common/validator.spec.ts
@@ -2,21 +2,52 @@ import { Validators } from './validators';
 
 describe('Form Validators', () => {
   test('email validation', () => {
-    const isValidEmail = (input: string) =>
+    const validate = (input: string) =>
       Validators.validate(input, [Validators.EmailAddressValidator]);
-    expect(isValidEmail('ss@')).toBeFalsy();
-    expect(isValidEmail('spencer@s.co')).toBeTruthy();
-    expect(isValidEmail('s@s.s.s.co')).toBeTruthy();
-    expect(isValidEmail('s+s@s.co')).toBeTruthy();
-    expect(isValidEmail('@s.co')).toBeFalsy();
+    expect(validate('ss@')).toBeFalsy();
+    expect(validate('spencer@s.co')).toBeTruthy();
+    expect(validate('s@s.s.s.co')).toBeTruthy();
+    expect(validate('s+s@s.co')).toBeTruthy();
+    expect(validate('@s.co')).toBeFalsy();
   });
+
   test('null validator', () => {
-    const isNullValid = (input: string) =>
+    const validate = (input: string) =>
       Validators.validate(input, [Validators.NotNullValidator]);
-    expect(isNullValid(' ')).toBeFalsy();
-    expect(isNullValid('  ')).toBeFalsy();
-    expect(isNullValid('\n')).toBeFalsy();
-    expect(isNullValid(' s ')).toBeTruthy();
+    expect(validate(' ')).toBeFalsy();
+    expect(validate('  ')).toBeFalsy();
+    expect(validate('\n')).toBeFalsy();
+    expect(validate(' s ')).toBeTruthy();
   });
-  
+
+  test('length validation', () => {
+    let validate = (input: string) =>
+      Validators.validate(input, [
+        Validators.LengthValidator({ minLength: 2, maxLength: 4 }),
+      ]);
+    expect(validate('a')).toBeFalsy();
+    expect(validate('ab')).toBeTruthy();
+    expect(validate(' a')).toBeFalsy();
+    validate = (input: string) =>
+      Validators.validate(input, [
+        Validators.LengthValidator({ minLength: 0, maxLength: 1 }),
+      ]);
+    expect(validate('')).toBeTruthy();
+    expect(validate('a')).toBeTruthy();
+    expect(validate('ab')).toBeFalsy();
+    validate = (input: string) =>
+      Validators.validate(input, [
+        Validators.LengthValidator({ maxLength: 1 }),
+      ]);
+    expect(validate('')).toBeTruthy();
+    expect(validate('a')).toBeTruthy();
+    expect(validate('ab')).toBeFalsy();
+    validate = (input: string) =>
+      Validators.validate(input, [
+        Validators.LengthValidator({ minLength: 1 }),
+      ]);
+    expect(validate('')).toBeFalsy();
+    expect(validate('a')).toBeTruthy();
+    expect(validate('abasdfasasfasdfasdf')).toBeTruthy();
+  });
 });
