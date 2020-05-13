de038c47a4998ab44813b4211458b2197f04d371
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-05-13 12:37:52 -0600

    refactored routes to allow for routes outside of the main template

--- src/routes/landing_page/landing_page.tsx
+++ src/routes/landing_page/landing_page.tsx
@@ -1,7 +1,20 @@
 import React, { useEffect, useContext } from 'react';
 import { FeedbackContext } from '../../context/feedback_context';
+import { Page, PageHeader, Brand } from '@patternfly/react-core';
 export const LandingPage = () => {
   const feedbackContext = useContext(FeedbackContext);
   useEffect(() => feedbackContext.hideLoader(), [feedbackContext]);
-  return <div />;
+  const pageHeader = (
+    <PageHeader
+      logo={<Brand alt="Patternfly Logo" />}
+      // toolbar={PageToolbar}
+      // avatar={<Avatar src={imgAvatar} alt="Avatar image" />}
+      // topNav={PageNav}
+    />
+  );
+  return (
+    <>
+      <Page header={pageHeader}></Page>
+    </>
+  );
 };
