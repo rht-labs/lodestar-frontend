38f73cb03f1b95023f209f07d551f9e21fcf88bc
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-05-14 11:08:00 -0600

    inverted hooks and contexts relationship to avoid future circular dependencies

--- src/context/config_context/config_hook.spec.tsx
+++ src/context/config_context/config_hook.spec.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { renderHook, cleanup, act } from '@testing-library/react-hooks';
 import { useConfig } from './config_hook';
 import { FakedConfigService } from '../../services/config_service/implementations/faked_config_service';
+import { TestStateWrapper } from '../../common/test_state_wrapper';
 
 describe('Config Context Hook', () => {
-  const getHook = () =>
-    renderHook(() => useConfig({ configRepository: new FakedConfigService() }));
+  const getHook = () => {
+    const wrapper = ({ children }) => (
+      <TestStateWrapper>{children}</TestStateWrapper>
+    );
+    return renderHook(() => useConfig(), { wrapper });
+  };
   afterEach(() => cleanup());
   test('Config is undefined by default', async () => {
     const { result } = getHook();
