1649a476a11fc8327b23b3ff2ee33a16b049e0d1
Venus Izadi <mizadi@redhat.com>
2020-06-11 12:40:31 -0400

    update the header based on new upgrade of patternfly

--- src/components/navigation/navigation_bar.tsx
+++ src/components/navigation/navigation_bar.tsx
@@ -1,77 +1,69 @@
 import React from 'react';
-import { UserDropdown } from './user_dropdown';
-import { Link, useLocation } from 'react-router-dom';
+import {UserDropdown} from './user_dropdown';
+import {Link, useLocation} from 'react-router-dom';
 import {Notification} from "./notification";
 import {
-  PageHeader,
   Brand,
-  Toolbar,
-  ToolbarItem,
-  ToolbarGroup,
   Nav,
   NavItem,
   NavList,
-  NavVariants,
+  PageHeader,
+  PageHeaderTools,
+  PageHeaderToolsGroup,
+  PageHeaderToolsItem
 } from '@patternfly/react-core';
 
-import {
-  HelpIcon
-} from '@patternfly/react-icons';
+import {HelpIcon} from '@patternfly/react-icons';
 
-export interface NavigationBarProps{
+export interface NavigationBarProps {
   isDrawerOpen: boolean;
-  onNavToggle: ()=>void;
+  onNavToggle: () => void;
 }
-export function NavigationBar(props:NavigationBarProps) {
+
+export function NavigationBar(props: NavigationBarProps) {
 
   const iconPad: React.CSSProperties = {
     paddingBottom: 0,
     paddingTop: 0,
   };
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
   return (
     <PageHeader
       showNavToggle
       isNavOpen={props.isDrawerOpen}
       onNavToggle={props.onNavToggle}
       logo={
-        <div>
-          <Toolbar>
-            <Brand
-              alt="Open Innovation Labs"
-              src={`${process.env.PUBLIC_URL}/oil_logo.png`}
-            />
-            <div style={{width: 50}}/>
-          </Toolbar>
-        </div>
+        <Brand
+          alt="Open Innovation Labs"
+          src={`${process.env.PUBLIC_URL}/oil_logo.png`}
+        />
       }
-      toolbar={
-        <Toolbar>
-          <ToolbarGroup>
-            <Notification />
-          </ToolbarGroup>
-
-          <ToolbarGroup>
-            <Nav theme="dark">
-              <NavList variant={NavVariants.horizontal}>
-                <NavItem
-                  id="aboutLink"
-                  itemId={3}
-                  isActive={pathname === '/about'}
-                >
-                  <Link style={iconPad} to="/app/about">
-                    <HelpIcon title="About" />
-                  </Link>
-                </NavItem>
-              </NavList>
-            </Nav>
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <ToolbarItem>
-              <UserDropdown />
-            </ToolbarItem>
-          </ToolbarGroup>
-        </Toolbar>
+      headerTools={
+        <PageHeaderTools>
+          <PageHeaderToolsGroup>
+            <PageHeaderToolsItem>
+              <Notification/>
+            </PageHeaderToolsItem>
+            <PageHeaderToolsItem>
+              <Nav theme="dark" variant="horizontal">
+                <NavList>
+                  <NavItem
+                    id="aboutLink"
+                    itemId={3}
+                    isActive={pathname === '/about'}
+                  >
+                    <Link style={iconPad} to="/app/about">
+                      <HelpIcon title="About"/>
+                    </Link>
+                  </NavItem>
+                </NavList>
+              </Nav>
+            </PageHeaderToolsItem>
+            <PageHeaderToolsItem>
+              <UserDropdown/>
+            </PageHeaderToolsItem>
+          </PageHeaderToolsGroup>
+        </PageHeaderTools>
       }
     />
   );
