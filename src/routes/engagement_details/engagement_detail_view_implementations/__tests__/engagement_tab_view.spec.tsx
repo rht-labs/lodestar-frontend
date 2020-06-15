5c4e38a03c3f65e0470e0c0da26a5f5a45ea4d23
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-06-15 11:27:47 -0600

    Convenient urls (#178)
    * null coalescing operator
    
    * added save button
    
    * fixes for failing tests
    
    * added convenient urls to enable direct navigation to specific tabs
    
    * updated tests
    
    * actually resolved merge conflicts
    
    * updated failing test from bad merge with develop

--- src/routes/engagement_details/engagement_detail_view_implementations/__tests__/engagement_tab_view.spec.tsx
+++ src/routes/engagement_details/engagement_detail_view_implementations/__tests__/engagement_tab_view.spec.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { EngagementTabView } from '../engagement_tab_view';
 import { TestStateWrapper } from '../../../../common/test_state_wrapper';
+import { MemoryRouter } from 'react-router';
 describe('Engagement Tab View', () => {
   test('should match snapshot', () => {
     expect(
       render(
-        <TestStateWrapper>
-          <EngagementTabView />
-        </TestStateWrapper>
+        <MemoryRouter>
+          <TestStateWrapper>
+            <EngagementTabView />
+          </TestStateWrapper>
+        </MemoryRouter>
       )
     ).toMatchSnapshot();
   });
