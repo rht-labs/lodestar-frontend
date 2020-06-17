8f82504f221bbc10a1093931ffefe2da40693acb
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-06-17 09:30:35 -0600

    added redirect to tab view to handle navigating to root status page

--- src/routes/engagement_details/engagement_detail_view_implementations/engagement_tab_view.tsx
+++ src/routes/engagement_details/engagement_detail_view_implementations/engagement_tab_view.tsx
@@ -4,7 +4,7 @@ import { Tabs, Tab } from '@patternfly/react-core';
 import { useEngagements } from '../../../context/engagement_context/engagement_hook';
 import { ClusterUsers } from '../form_views/04_cluster_users';
 import { EditPaneWrapper } from '../../../components/edit_pane_wrapper/edit_pane_wrapper';
-import { useLocation, useHistory } from 'react-router';
+import { useLocation, useHistory, Redirect, useParams } from 'react-router';
 import { EngagementOverviewTab } from '../tab_views/overview';
 import { HostingEnvironmentTab } from '../tab_views/hosting_environment';
 interface EngagementTabViewProps extends EngagementViewProps {}
@@ -25,6 +25,7 @@ export function EngagementTabView({ engagement }: EngagementTabViewProps) {
   } = useEngagements();
   const { pathname } = useLocation();
   const history = useHistory();
+  const { customer_name, project_name } = useParams();
   const handleTabSelect = (_, tabIndex) => {
     history.push(
       `/app/engagements/${engagementFormState.customer_name}/${engagementFormState.project_name}/${tabIndex}`
@@ -38,47 +39,58 @@ export function EngagementTabView({ engagement }: EngagementTabViewProps) {
     if (activeTab) {
       return TabNames[activeTab];
     }
-    return TabNames.basicInfo;
+    return TabNames.overview;
   }
 
   return (
-    <Tabs isBox activeKey={getActiveKey()} onSelect={handleTabSelect}>
-      <Tab title="Basic Information" eventKey={TabNames.overview} id="overview">
-        <TabContentWrapper>
-          <EngagementOverviewTab
-            formOptions={formOptions}
-            onChange={updateEngagementFormField}
-            engagement={engagementFormState}
-          />
-        </TabContentWrapper>
-      </Tab>
-      <Tab title="Users" eventKey={TabNames.users} id="users">
-        <TabContentWrapper>
-          <EditPaneWrapper engagement={engagementFormState}>
-            <ClusterUsers
+    <>
+      <Redirect
+        exact
+        from="/app/engagements/:customer_name/:project_name"
+        to={`/app/engagements/${customer_name}/${project_name}/${TabNames.basicInfo}`}
+      />
+      <Tabs isBox activeKey={getActiveKey()} onSelect={handleTabSelect}>
+        <Tab
+          title="Basic Information"
+          eventKey={TabNames.basicInfo}
+          id="overview"
+        >
+          <TabContentWrapper>
+            <EngagementOverviewTab
               formOptions={formOptions}
+              onChange={updateEngagementFormField}
               engagement={engagementFormState}
-              onChange={updateEngagementFormField}
             />
-          </EditPaneWrapper>
-        </TabContentWrapper>
-      </Tab>
-      <Tab
-        title="Hosting Environment"
-        eventKey={TabNames.hostingEnvironment}
-        id={'hosting_environment'}
-      >
-        <TabContentWrapper>
-          <EditPaneWrapper engagement={engagementFormState}>
-            <HostingEnvironmentTab
-              formOptions={formOptions}
-              engagement={engagement}
-              onChange={updateEngagementFormField}
-            />
-          </EditPaneWrapper>
-        </TabContentWrapper>
-      </Tab>
-    </Tabs>
+          </TabContentWrapper>
+        </Tab>
+        <Tab title="Users" eventKey={TabNames.users} id="users">
+          <TabContentWrapper>
+            <EditPaneWrapper engagement={engagementFormState}>
+              <ClusterUsers
+                formOptions={formOptions}
+                engagement={engagementFormState}
+                onChange={updateEngagementFormField}
+              />
+            </EditPaneWrapper>
+          </TabContentWrapper>
+        </Tab>
+        <Tab
+          title="Hosting Environment"
+          eventKey={TabNames.hostingEnvironment}
+          id={'hosting_environment'}
+        >
+          <TabContentWrapper>
+            <EditPaneWrapper engagement={engagementFormState}>
+              <HostingEnvironmentTab
+                formOptions={formOptions}
+                engagement={engagement}
+                onChange={updateEngagementFormField}
+              />
+            </EditPaneWrapper>
+          </TabContentWrapper>
+        </Tab>
+      </Tabs>
+    </>
   );
 }
 
