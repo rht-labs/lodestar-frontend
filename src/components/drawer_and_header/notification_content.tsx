f3a16a84b38c95205310ed1b779e6b1a78990972
Venus Izadi <mizadi@redhat.com>
2020-06-18 23:44:58 -0400

    add notification content

--- src/components/drawer_and_header/notification_content.tsx
+++ src/components/drawer_and_header/notification_content.tsx
@@ -4,6 +4,7 @@ import {
   DrawerCloseButton,
   DrawerHead,
   DrawerPanelContent,
+  Alert, AlertActionCloseButton, AlertActionLink,
 } from '@patternfly/react-core';
 
 export interface NotificationContentProps{
@@ -15,10 +16,34 @@ export function NotificationContent(props: NotificationContentProps) {
   return (
     <DrawerPanelContent>
       <DrawerHead>
-        <span>drawer-panel</span>
         <DrawerActions>
           <DrawerCloseButton onClick={props.onClose}/>
         </DrawerActions>
       </DrawerHead>
+      <Alert title="Default alert title">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante
+      </Alert>
+      <Alert
+        variant="success"
+        title="Success alert title"
+        actionClose={<AlertActionCloseButton />}
+        actionLinks={
+          <>
+            <AlertActionLink >View details</AlertActionLink>
+            <AlertActionLink>Ignore</AlertActionLink>
+          </>
+        }
+      >
+        <p>Success alert description. </p>
+      </Alert>
+      <Alert variant="info" title="Info alert title" >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante
+      </Alert>
+      <Alert variant="warning" title="Warning alert title" >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante
+      </Alert>
+      <Alert variant="danger" title="Danger alert title" >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante
+      </Alert>
     </DrawerPanelContent>
   )}
