121055a67b6d5d6c34f1dfbbb2d84baf2991da1b
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-06-24 11:25:12 -0600

    added reset button

--- src/components/engagement_filter_bar/engagement_filter_bar.tsx
+++ src/components/engagement_filter_bar/engagement_filter_bar.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { TextInput, Grid, GridItem } from '@patternfly/react-core';
+import {
+  TextInput,
+  Grid,
+  GridItem,
+  Button,
+  ButtonVariant,
+} from '@patternfly/react-core';
 import { EngagementFilter } from '../../schemas/engagement_filter';
 import { SortSelect } from './components/sort_select';
 import { EngagementStatusSelect } from './components/engagement_status_select';
@@ -13,19 +19,26 @@ export function EngagementFilterBar({
   onChange,
   filter,
 }: EngagementFilterProps) {
+  const { searchTerm = '' } = filter ?? {};
   return (
     <Grid hasGutter>
       <GridItem span={4}>
         <TextInput
+          value={searchTerm}
           onChange={searchTerm => onChange({ ...filter, searchTerm })}
           placeholder="Search for an engagement"
-        ></TextInput>
+        />
       </GridItem>
       <GridItem span={3}>
         <EngagementStatusSelect onChange={onChange} filter={filter} />
       </GridItem>
       <GridItem span={3}>
         <SortSelect onChange={onChange} filter={filter} />
+      </GridItem>
+      <GridItem span={2}>
+        <Button variant={ButtonVariant.secondary} onClick={() => onChange({})}>
+          Reset
+        </Button>
       </GridItem>
     </Grid>
   );
