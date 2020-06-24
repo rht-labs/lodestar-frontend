c19beb56c885ebc3d26f9f36be5d8b4d0be4c998
Venus Izadi <mizadi@redhat.com>
2020-06-24 11:24:11 -0400

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 
-import {BrowserRouter as Router} from 'react-router-dom';
-import {SessionProvider} from './context/session_context/session_context';
-import {VersionProvider} from './context/version_context/version_context';
-import {ConfigProvider} from './context/config_context/config_context';
-import {EngagementProvider} from './context/engagement_context/engagement_context';
-import {ErrorBoundary} from './components/error_boundary';
-import {OMPRouter} from './routes/router';
-import {FeatureToggles} from './context/feature_toggles/feature_toggles';
-import {ServiceProvider} from './context/service_provider_context/service_provider_context';
-import {FeedbackProvider} from './context/feedback_context';
-import {PublicConfigService} from './services/config_service/implementations/public_config_service';
-import {GlobalLoadingProvider} from './context/global_loading_context/global_loading_context';
-import {NotificationProvider} from "./context/notification_context/notification_context";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { SessionProvider } from './context/session_context/session_context';
+import { VersionProvider } from './context/version_context/version_context';
+import { ConfigProvider } from './context/config_context/config_context';
+import { EngagementProvider } from './context/engagement_context/engagement_context';
+import { ErrorBoundary } from './components/error_boundary';
+import { OMPRouter } from './routes/router';
+import { FeatureToggles } from './context/feature_toggles/feature_toggles';
+import { ServiceProvider } from './context/service_provider_context/service_provider_context';
+import { FeedbackProvider } from './context/feedback_context';
+import { PublicConfigService } from './services/config_service/implementations/public_config_service';
+import { GlobalLoadingProvider } from './context/global_loading_context/global_loading_context';
+import { NotificationProvider } from './context/notification_context/notification_context';
 
 export const App = () => {
   return (
@@ -28,7 +28,7 @@ export const App = () => {
                     <Router>
                       <EngagementProvider>
                         <GlobalLoadingProvider>
-                          <OMPRouter/>
+                          <OMPRouter />
                         </GlobalLoadingProvider>
                       </EngagementProvider>
                     </Router>
