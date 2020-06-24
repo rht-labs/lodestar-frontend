99311d15af90574f1f9a1e722efc2f34b10eca96
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-06-24 15:23:24 -0600

    added cluster launch feedback

--- src/components/launch_alert_banner/launch_alert_banner.tsx
+++ src/components/launch_alert_banner/launch_alert_banner.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import { Engagement } from '../../schemas/engagement_schema';
 import { Alert, AlertVariant, Button } from '@patternfly/react-core';
-
-interface LaunchMessageProps {
+import { format } from 'date-fns';
+interface LaunchAlertBannerProps {
   engagement: Engagement;
   onLaunch: (engagement: Engagement) => void;
 }
-export function LaunchMessage({ engagement, onLaunch }: LaunchMessageProps) {
-  if (!engagement?.launch) {
-    return (
-      <Alert
-        isInline
-        title="Engagement not launched"
-        variant={AlertVariant.info}
-        actionLinks={
+export function LaunchAlertBanner({
+  engagement,
+  onLaunch,
+}: LaunchAlertBannerProps) {
+  return (
+    <Alert
+      isInline
+      title={
+        engagement?.launch ? 'Engagement Launched' : 'Engagement Not Launched'
+      }
+      variant={AlertVariant.info}
+      actionLinks={
+        !engagement?.launch ? (
           <>
             <Button onClick={() => onLaunch(engagement)}>Launch</Button>
           </>
-        }
-      >
-        This engagement has not been launched
-      </Alert>
-    );
+        ) : (
+          undefined
+        )
+      }
+    >
+      <LaunchMessage engagement={engagement} />
+    </Alert>
+  );
+}
+
+interface LaunchMessageProps {
+  engagement: Engagement;
+}
+export function LaunchMessage({ engagement }: LaunchMessageProps) {
+  let launchMessage = '';
+  if (!engagement?.launch) {
+    launchMessage = 'This engagement has not been launched';
+  } else {
+    launchMessage = `This engagement was launched on ${format(engagement?.launch?.launched_date_time, 'MMM d, yyyy')} by ${engagement?.launch?.launched_by}`;
   }
-  return <div />;
+  return <span>{launchMessage}</span>;
 }
