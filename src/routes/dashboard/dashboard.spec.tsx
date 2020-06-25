a1f379190ca117a571c67eaf7e918592e7c877b4
Venus Izadi <mizadi@redhat.com>
2020-06-25 12:53:37 -0400

    fix the test

--- src/routes/dashboard/dashboard.spec.tsx
+++ src/routes/dashboard/dashboard.spec.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Dashboard } from '.';
 import { render } from '@testing-library/react';
+import {TestStateWrapper} from "../../common/test_state_wrapper";
+import {MemoryRouter} from 'react-router';
 
 describe('Dashboard Component', () => {
   test('matches snapshot', () => {
-    expect(render(<Dashboard />)).toMatchSnapshot();
+    expect(render(
+      <MemoryRouter>
+      <TestStateWrapper>
+        <Dashboard/>
+      </TestStateWrapper>
+      </MemoryRouter>)
+    ).toMatchSnapshot();
   });
 });
