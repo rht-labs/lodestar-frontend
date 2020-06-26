540649bda2859ab13b38b833d57b179b3247d9dd
Venus Izadi <mizadi@redhat.com>
2020-06-26 18:29:46 -0400

    add system status data to be displayed

--- src/components/engagement_data_cards/system_status_card/system_status_card.tsx
+++ src/components/engagement_data_cards/system_status_card/system_status_card.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import {DataCard} from '../data_card';
 import {Engagement} from '../../../schemas/engagement_schema';
 import {Button, ButtonVariant, Grid, GridItem} from '@patternfly/react-core';
-import {CheckCircleIcon, ExclamationTriangleIcon} from "@patternfly/react-icons";
+import {SubsystemDetails} from "./subsystem_details";
 
 export interface SystemStatusCardProps {
   engagement: Engagement;
 }
 
 export function SystemStatusCard({engagement}: SystemStatusCardProps) {
-
   return (
     <>
       <DataCard
@@ -23,26 +22,14 @@ export function SystemStatusCard({engagement}: SystemStatusCardProps) {
         title="System Status"
       >
         <Grid hasGutter>
-          <GridItem span={2}>
-            <ExclamationTriangleIcon color="#EC7A08"/>
-            Atlassian
-          </GridItem>
-          <GridItem span={2}>
-            <CheckCircleIcon color="green"/>
-            IBM Cloud
-          </GridItem>
-          <GridItem span={2}>
-            <ExclamationTriangleIcon color="#EC7A08"/>
-            Mural
-          </GridItem>
-          <GridItem span={2}>
-            <ExclamationTriangleIcon color="#C9190B"/>
-            Red Hat SSO
-          </GridItem>
-          <GridItem span={2}>
-            <ExclamationTriangleIcon color="#EC7A08"/>
-            Openshift Cluster
-          </GridItem>
+          {engagement?.status?.subsystems.map( subsystem => (
+            <GridItem span={2}>
+              <SubsystemDetails
+                title={subsystem.name}
+                status={subsystem.status}
+              />
+            </GridItem>
+          ))}
         </Grid>
       </DataCard>
     </>
