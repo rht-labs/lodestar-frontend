8ed62a8d53ed7d00566d8f7241a59d2f4f0313ae
Venus Izadi <mizadi@redhat.com>
2020-06-26 11:31:06 -0400

    update the schema and the test faker

--- src/schemas/engagement_schema.ts
+++ src/schemas/engagement_schema.ts
@@ -56,13 +56,12 @@ export abstract class Engagement {
       start_date: faker.date.recent(),
       technical_lead_email: faker.internet.email(),
       technical_lead_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      creation_details: faker.random.boolean()
-        ? {
+      creation_details:
+        {
           created_by_email: faker.internet.email(),
           created_by_user: `${faker.name.firstName()} ${faker.name.lastName()}`,
           created_on: faker.date.recent(),
-        }
-        : null,
+        },
       launch: faker.random.boolean()
         ? {
             launched_by: faker.name.firstName(),
