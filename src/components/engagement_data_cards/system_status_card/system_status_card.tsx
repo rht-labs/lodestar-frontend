3e7ce932fd19b03ed4b54e8f68b0e7867df7781b
Venus Izadi <mizadi@redhat.com>
2020-06-29 10:15:12 -0400

    add messages to the subsystem click

--- src/components/engagement_data_cards/system_status_card/system_status_card.tsx
+++ src/components/engagement_data_cards/system_status_card/system_status_card.tsx
@@ -3,23 +3,38 @@ import {DataCard} from '../data_card';
 import {Button, ButtonVariant, Grid, GridItem} from '@patternfly/react-core';
 import {SubsystemDetails} from "./subsystem_details";
 import {useEngagements} from "../../../context/engagement_context/engagement_hook";
+import {Engagement} from "../../../schemas/engagement_schema";
+import {SystemStatusDetailsModal} from "./system_status_details_modal";
+import {useModalVisibility} from "../../../context/edit_modal_visibility_context/edit_modal_visibility_hook";
 
-export function SystemStatusCard() {
-  const { activeEngagement } = useEngagements();
+export interface SystemStatusCardProps{
+  engagement: Engagement;
+};
+const SYSTEM_STATUS_MODAL_KEY = 'system_status';
+
+export function SystemStatusCard({
+  engagement,
+}: SystemStatusCardProps) {
+  const { activeEngagement: { status } = {} } = useEngagements();
+  const { requestOpen, activeModalKey } = useModalVisibility();
   return (
     <>
+      <SystemStatusDetailsModal
+        isOpen={activeModalKey?.includes(SYSTEM_STATUS_MODAL_KEY)}
+      />
       <DataCard
         actionButton={() => (
           <Button
             variant={ButtonVariant.link}
+            onClick={() => requestOpen(SYSTEM_STATUS_MODAL_KEY)}
           >
-            Edit
+            View More
           </Button>
         )}
         title="System Status"
       >
-        <Grid hasGutter>
-          {activeEngagement?.status?.subsystems.map( subsystem => (
+        <Grid>
+          {status?.subsystems.map( subsystem => (
             <GridItem span={2}>
               <SubsystemDetails
                 title={subsystem.name}
