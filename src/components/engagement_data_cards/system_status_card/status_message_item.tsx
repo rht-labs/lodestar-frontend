750236165873692cbe83618e6a7f9ff7c9c0aec6
Venus Izadi <mizadi@redhat.com>
2020-06-29 10:15:12 -0400

    add messages to the subsystem click

---
+++ src/components/engagement_data_cards/system_status_card/status_message_item.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Severity} from "../../../schemas/system_message";
+import {
+  DataList,
+  DataListCell,
+  DataListItem,
+  DataListItemCells,
+  DataListItemRow
+} from "@patternfly/react-core";
+import { format as formatDate, isValid } from 'date-fns';
+
+export interface StatusMessageItemProps{
+  severity: Severity,
+  message: string;
+  updated: Date;
+}
+
+export function StatusMessageItem({
+  severity,
+  message,
+  updated
+}: StatusMessageItemProps) {
+
+  const severityBox = {
+    display: 'block',
+    width: '100%',
+    padding: '.25rem .5rem',
+    color: '#004e8a',
+    backgroundColor: '#def3ff',
+    border: '1px solid rgba(0,0,0,.1)',
+    borderRadius: '4px'
+  };
+
+  return (
+    <>
+      <DataList aria-label="system status message" isCompact>
+        <DataListItem aria-labelledby="system-status-messages">
+          <DataListItemRow>
+            <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="severity" >
+                    <div style={severityBox}>
+                      Severity: {severity}
+                    </div>
+                  </DataListCell>,
+                  <DataListCell key="message" width={5}>
+                    <span> {message ? message : 'No message'} </span>
+                  </DataListCell>,
+                  <DataListCell key="updated">
+                    <span>
+                    {!!updated && isValid(updated)
+                      ? formatDate(updated, 'MMM dd, yyyy')
+                      : ' - '}
+                    </span>
+                  </DataListCell>,
+                ]}
+            />
+          </DataListItemRow>
+        </DataListItem>
+      </DataList>
+    </>
+  );
+}
