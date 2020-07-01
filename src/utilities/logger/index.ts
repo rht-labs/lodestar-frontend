2b44afdbec9b2958154d99147f253ecdb811e62c
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-07-01 10:44:34 -0600

    updated logger

--- src/utilities/logger/index.ts
+++ src/utilities/logger/index.ts
@@ -1,18 +1,33 @@
 import { Logger as LoggerContract, LogVerbosity } from './logger';
 import { ConsoleLogger, SilentLogger } from './loggers';
 
-const LOGGERS = {
+const LOGGERS = (logVerbosity: LogVerbosity) => ({
   silent: SilentLogger,
-  console: ConsoleLogger,
-};
+  console: ConsoleLogger(logVerbosity),
+});
+
+const ENV_LOG_VERBOSITY: LogVerbosity = (function() {
+  const envVerbosityLevel = process.env.REACT_APP_LOG_VERBOSITY;
+  switch (envVerbosityLevel) {
+    case 'error':
+      return LogVerbosity.error;
+    case 'info':
+      return LogVerbosity.info;
+    case 'debug':
+      return LogVerbosity.debug;
+    default:
+      return LogVerbosity.error;
+  }
+})();
 
 export const Logger: LoggerContract = (function(): LoggerContract {
   const envLogger = process.env.REACT_APP_LOGGER ?? '';
-  if (envLogger in LOGGERS) {
-    return LOGGERS[envLogger];
+  const loggers = LOGGERS(ENV_LOG_VERBOSITY);
+  if (envLogger in loggers) {
+    return loggers[envLogger];
   } else if (process.env.NODE_ENV === 'production') {
-    return ConsoleLogger(LogVerbosity.error);
+    return loggers.console;
   } else {
-    return ConsoleLogger(LogVerbosity.debug);
+    return loggers.console;
   }
 })();
