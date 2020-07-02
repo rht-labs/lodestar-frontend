28e5ce030e2aa63a57a093e0f9ebd0715a9ed3be
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-07-02 07:34:19 -0600

    Feature/additional details (#231)
    * invalid date catch in patternfly date adapter
    
    * added null end_date check when calculating retirement date
    
    * fixed date instance check
    
    * reduced useEngagement dependencies
    
    * renamed engagementFormState
    
    * renamed activeEngagement
    
    * updated static mocks with new interface properties
    
    * removed facade
    
    * fixed state error in checkLaunchReady
    
    * fixes for logging. Removed unused files
    
    * updated logger
    
    * updated logger references
    
    * updated engagement tests
    
    * additional details
    
    * changed file and function name to match field name

---
+++ src/components/engagement_form_fields/additional_details.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useFeatures } from '../../context/feature_toggles/feature_hook';
+import { FormGroup, TextArea } from '@patternfly/react-core';
+import { APP_FEATURES } from '../../common/app_features';
+import { Engagement } from '../../schemas/engagement_schema';
+
+export interface DescriptionFormFieldProps {
+  engagement: Engagement;
+  onChange: (fieldName: string, value: any) => void;
+}
+
+export function AdditionalDetailsFormField(props: DescriptionFormFieldProps) {
+  const { hasFeature } = useFeatures();
+  return (
+    <FormGroup label="Additional Information" fieldId="additional_details">
+      <TextArea
+        disabled={!hasFeature(APP_FEATURES.writer)}
+        name="additional_details"
+        id="additional_details"
+        aria-label="Additional provisioning information"
+        placeholder="Provide additional details"
+        value={props.engagement.additional_details || ''}
+        resizeOrientation="vertical"
+        onChange={e => props.onChange('additional_details', e)}
+      />
+    </FormGroup>
+  );
+}
