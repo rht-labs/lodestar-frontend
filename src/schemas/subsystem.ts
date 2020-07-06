24b54cfcd554c16a4a5eaded449219a3b4b76a24
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-07-06 11:20:29 -0600

    changes to status card widgets (#234)
    * changes to status card widgets
    
    * added access links

--- src/schemas/subsystem.ts
+++ src/schemas/subsystem.ts
@@ -2,40 +2,59 @@ import { SystemMessage } from './system_message';
 import { HealthStatus } from './cluster_status';
 import faker from 'faker/locale/en_US';
 
+export interface AccessLink {
+  url: string;
+  title: string;
+}
+
 export interface Subsystem {
   name: string;
   status: HealthStatus;
   state: string;
   info: string;
   updated: Date;
-  web_console: string;
-  api: string;
   messages: SystemMessage[];
+  access_urls: AccessLink[];
 }
-const fakeSubsystemNames = ['Openshift Cluster', 'Atlassian', 'IBM Cloud', 'Mural', 'RH SSO'];
+const fakeSubsystemNames = [
+  'Openshift Cluster',
+  'Atlassian',
+  'IBM Cloud',
+  'Mural',
+  'RH SSO',
+];
 
 export abstract class Subsystem {
   static fromFake(): Subsystem {
     return {
-      name: fakeSubsystemNames[faker.random.number(fakeSubsystemNames.length - 1)],
-      status: HealthStatus[HealthStatus[faker.random.number(Object.keys(HealthStatus).length - 1)]],
+      name:
+        fakeSubsystemNames[faker.random.number(fakeSubsystemNames.length - 1)],
+      status:
+        HealthStatus[
+          Object.keys(HealthStatus)[
+            faker.random.number(Object.keys(HealthStatus).length - 1)
+          ]
+        ],
       state: 'provisioned',
+      access_urls: Array.apply(null, new Array(3)).map(_ => ({
+        title: faker.lorem.word(),
+        url: faker.internet.url(),
+      })),
       info: faker.lorem.sentence(),
       updated: faker.date.recent(),
-      web_console: faker.internet.url(),
-      api: faker.internet.url(),
-      messages: SystemMessage[SystemMessage[faker.random.number(3)]],
+      messages: Array.apply(null, new Array(5)).map(_ =>
+        SystemMessage.fromFake()
+      ),
     };
   }
   static staticFaked(): Subsystem {
     return {
       name: 'Openshift',
       status: HealthStatus.yellow,
-      state: "provisioned",
+      access_urls: [],
+      state: 'provisioned',
       info: 'Deployment In Progress',
       updated: new Date(2020, 1, 1),
-      web_console: 'https://console......',
-      api: 'https://api.....:6443',
       messages: [SystemMessage.staticFaked()],
     };
   }
