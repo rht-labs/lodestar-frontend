43d185b230bff3f726b93e436948edc1395a00e2
Venus Izadi <mizadi@redhat.com>
2020-07-16 08:58:52 -0400

    e2e - edit points of contact

--- cypress/integration/engagement_spec.js
+++ cypress/integration/engagement_spec.js
@@ -10,7 +10,7 @@ describe('Login', () => {
 
 describe('new engagement', () => {
 
-  it('creates a new engagement and edit the details', () => {
+  it('creates a new engagement', () => {
 
     cy.server();
     cy.route({method: 'POST', url: 'engagements'}).as('createCheck');
@@ -44,18 +44,67 @@ describe('new engagement', () => {
     // cy.get('li > .pf-c-alert')
     //     .contains('Your engagement has been successfully created');
 
+    // Edit Engagement Summary
     cy.get('button[data-cy=edit_summary_card]')
         .click();
 
-    // cy.contains('Point of Contact').click();
-    //
-    // cy.get('input[name=engagement-lead-name]').type('Morgan Reilly');
-    // cy.get('input[name=engagement-lead-email]').type('morgan@redhat.com');
-    // cy.get('input[name=tech-lead-name]').type('Jake Muzzin');
-    // cy.get('input[name=tech-lead-email]').type('jake@redhat.com');
-    // cy.get('input[name=customer-contact-name]').type('Andreas Johnsson');
-    // cy.get('input[name=customer-contact-email]').type('andreas@redhat.com');
-    //
+    cy.get('textarea[data-cy=description_field]')
+        .type('Herp derpsum derp herpy le nerpy terp jerpy derpy.')
+        .get('input[data-cy=location_field]')
+        .type('Katmandu, Nepal')
+        .get('input[data-cy=start_date_input]')
+        .type('2020-10-25')
+        .get('input[data-cy=end_date_input]')
+        .type('2020-12-25');
+
+    cy.get('button[data-cy=save_summary_card]')
+        .click();
+    cy.get('li > .pf-c-alert')
+        .contains('Your updates have been successfully saved.')
+
+    cy.get('.pf-c-alert__action > .pf-c-button')
+        .click();
+
+    // Edit points of contact
+    cy.get('[data-cy="points_of_contact"]')
+        .click();
+
+    cy.get('[data-cy="engagement_lead_name"]')
+        .type('Test EL')
+        .get('[data-cy=engagement_lead_email]')
+        .type('test.el@redhat.com')
+        .get('[data-cy=tech_lead_name]')
+        .type('Test tech')
+        .get('[data-cy=tech_lead_email]')
+        .type('test.tech@redhat.com')
+        .get('[data-cy=customer_contact_name]')
+        .type('Test Customer')
+        .get('[data-cy=customer_contact_email]')
+        .type('test@customer.com');
+
+    cy.get('[data-cy=save_point_of_contact]')
+        .click();
+
+    // Edit points of contact
+    cy.get('[data-cy="points_of_contact"]')
+        .click();
+
+    cy.get('[data-cy="engagement_lead_name"]')
+        .type('Test EL')
+        .get('[data-cy=engagement_lead_email]')
+        .type('test.el@redhat.com')
+        .get('[data-cy=tech_lead_name]')
+        .type('Test tech')
+        .get('[data-cy=tech_lead_email]')
+        .type('test.tech@redhat.com')
+        .get('[data-cy=customer_contact_name]')
+        .type('Test Customer')
+        .get('[data-cy=customer_contact_email]')
+        .type('test@customer.com');
+
+    cy.get('[data-cy=save_point_of_contact]')
+        .click();
+
     // cy.contains('OpenShift Cluster').click();
     //
     // cy.get('[aria-label="Cloud provider region"]')
@@ -94,57 +143,4 @@ describe('new engagement', () => {
     //
     // cy.get('[data-cy=launch]').should('be.disabled');
   });
-  it('Edit the Summary details', () => {
-
-    cy.get('textarea[data-cy=description_field]')
-        .type('Herp derpsum derp herpy le nerpy terp jerpy derpy.')
-        .get('input[data-cy=location_field]')
-        .should('have.value', '')
-        .type('Katmandu, Nepal')
-        .get('input[data-cy=start_date_input]')
-        .type('2020-10-25')
-        .get('input[data-cy=end_date_input]')
-        .type('2020-12-25');
-
-    //todo: needs to come back to this engagement and assert if the values have been updated
-    // cy.get('button[data-cy=save_summary_card]')
-    //     .click();
-    //
-    // cy.get('[data-cy=description_label]')
-    //     .should('have.value', 'Herp derpsum derp herpy le nerpy terp jerpy derpy.');
-    //
-    // cy.get('[data-cy=location_label]')
-    //     .should('have.value', 'Katmandu, Nepal');
-    //
-    // cy.get('[data-cy=start_date_label]')
-    //     .should('have.value', '2020-10-25');
-    //
-    // cy.get('[data-cy=end_date_label]')
-    //     .should('have.value', '2020-12-25');
-
-  });
-
-  it('Edit the points of contact', () => {
-    cy.get('button[data-cy=points_of_contact]')
-        .click();
-  });
 });
-
-// describe('new engagement', () => {
-//
-//   it('Search for the newly added engagement, and edit that', () => {
-//     cy.server();
-//     cy.route({
-//       method: 'PUT',
-//       url: 'engagements/customers/e2e/projects/cypressio',
-//     }).as('saveCheck');
-//
-//     cy.request('/app/dashboard');
-//
-//     cy.get('#nav-toggle')
-//         .click();
-//
-//     cy.contains('Create New').click();
-//   })
-// });
-
