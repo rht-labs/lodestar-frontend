6c18f0d7e052eedb9a0ddcb213672bdad302b0b6
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-08-18 12:09:20 -0600

    add section specific link for the missing field warning

--- src/components/launch_alert_banner/launch_alert_banner.tsx
+++ src/components/launch_alert_banner/launch_alert_banner.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Engagement } from '../../schemas/engagement';
-import { Alert, AlertVariant, Button } from '@patternfly/react-core';
+import { Alert, AlertVariant, Button, Title } from '@patternfly/react-core';
 import { format } from 'date-fns';
 import { RequiredFieldsWarning } from '../required_fields_warning/required_fields_warning';
 import { HealthStatus } from '../../schemas/cluster_status';
+import { useLocation } from 'react-router';
+import { HashLink } from 'react-router-hash-link';
 interface LaunchAlertBannerProps {
   engagement: Engagement;
   onLaunch: (engagement: Engagement) => void;
@@ -11,6 +13,34 @@ interface LaunchAlertBannerProps {
   requiredFields: string[];
   missingRequiredFields: string[];
 }
+const ENGAGEMENT_CARDS = {
+  engagement_summary_card: 'Engagement Summary Card',
+  poc_card: 'Point of Contact',
+  oc_summary_card: 'Hosting Environment',
+  user_card: 'Users',
+};
+const ENGAGEMENT_FIELD_MAP = {
+  description: 'engagement_summary_card',
+  location: 'engagement_summary_card',
+  start_date: 'engagement_summary_card',
+  end_date: 'engagement_summary_card',
+  launch: 'engagement_summary_card',
+  archive_date: 'engagement_summary_card',
+  engagement_users: 'user_card',
+  engagement_lead_name: 'poc_card',
+  engagement_lead_email: 'poc_card',
+  technical_lead_name: 'poc_card',
+  technical_lead_email: 'poc_card',
+  customer_contact_name: 'poc_card',
+  customer_contact_email: 'poc_card',
+  ocp_cloud_provider_name: 'oc_summary_card',
+  ocp_cloud_provider_region: 'oc_summary_card',
+  ocp_version: 'oc_summary_card',
+  ocp_sub_domain: 'oc_summary_card',
+  ocp_persistent_storage_size: { hash: 'oc_summary_card' },
+  ocp_cluster_size: 'oc_summary_card',
+  suggested_subdomain: 'oc_summary_card',
+};
 export function LaunchAlertBanner({
   engagement,
   onLaunch,
@@ -19,6 +49,7 @@ export function LaunchAlertBanner({
   missingRequiredFields,
 }: LaunchAlertBannerProps) {
   const overallStatus = engagement?.status?.overall_status;
+  const path = useLocation().pathname;
   return (
     <Alert
       isInline
@@ -29,19 +60,41 @@ export function LaunchAlertBanner({
       actionLinks={
         !engagement?.launch ? (
           <div>
-            <Button
-              isDisabled={!isLaunchable}
-              onClick={() => onLaunch(engagement)}
-              data-cy={'launch_button'}
-            >
-              Launch
-            </Button>
-            <span style={{ margin: '0 0.5rem' }}>
-              <RequiredFieldsWarning
-                missingRequiredFields={missingRequiredFields}
-                requiredFields={requiredFields}
-              />
-            </span>
+            <div>
+              <Button
+                isDisabled={!isLaunchable}
+                onClick={() => onLaunch(engagement)}
+                data-cy={'launch_button'}
+              >
+                Launch
+              </Button>
+              <span style={{ margin: '0 0.5rem' }}>
+                <RequiredFieldsWarning
+                  missingRequiredFields={missingRequiredFields}
+                  requiredFields={requiredFields}
+                />
+              </span>
+            </div>
+            <div style={{ margin: '0.5rem 0' }}>
+              <span>
+                Required fields are missing in the following sections:
+              </span>
+              <ul>
+                {Array.from(
+                  new Set(
+                    missingRequiredFields.map(
+                      field => ENGAGEMENT_FIELD_MAP[field]
+                    )
+                  )
+                ).map(section => (
+                  <li>
+                    <HashLink smooth to={`${path}#${section}`}>
+                      {ENGAGEMENT_CARDS[section]}
+                    </HashLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         ) : (
           undefined
--- src/components/required_fields_warning/required_fields_warning.tsx
+++ src/components/required_fields_warning/required_fields_warning.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {
-  ExclamationTriangleIcon,
-  CheckCircleIcon,
-} from '@patternfly/react-icons';
-import { Tooltip, Popover } from '@patternfly/react-core';
+import { CheckCircleIcon } from '@patternfly/react-icons';
+import { Tooltip } from '@patternfly/react-core';
 import { getHumanReadableFieldName } from '../../common/human_readable_engagement_field';
 interface RequiredFieldsWarningProps {
   requiredFields: string[];
@@ -18,38 +15,35 @@ export function RequiredFieldsWarning({
     .map(field => getHumanReadableFieldName(field));
   if (!neededFields?.length) {
     return (
-      <Tooltip content="All required fields are completed"
-               entryDelay={10}
-               exitDelay={10}>
+      <Tooltip
+        content="All required fields are completed"
+        entryDelay={10}
+        exitDelay={10}
+      >
         <CheckCircleIcon color="green" />
       </Tooltip>
     );
   }
-  const tooltipText = neededFields.join(', ');
   return (
-    <Popover
-      headerContent="Missing required fields"
-      aria-label="Missing fields warning"
-      bodyContent={
-        <div style={{ padding: '0 0 0 2rem' }}>
-          <ul style={{ listStyleType: 'disc' }}>
-            {neededFields.map(f => (
-              <li key={f.toString()}>{f}</li>
-            ))}
-          </ul>
-        </div>
-      }
-    >
-      <span>
-        <Tooltip content={`Missing required fields: ${tooltipText}`}
-                 entryDelay={10}
-                 exitDelay={10}>
-          <ExclamationTriangleIcon
-            style={{ cursor: 'pointer' }}
-            color="#EC7A08"
-          />
-        </Tooltip>
-      </span>
-    </Popover>
+    <span>
+      <Tooltip
+        content={
+          <div>
+            <div>Missing required fields:</div>
+            <div>
+              <ul>
+                {neededFields.map(field => (
+                  <li>{field}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        }
+        entryDelay={10}
+        exitDelay={10}
+      >
+        <CheckCircleIcon color="lightgrey" />
+      </Tooltip>
+    </span>
   );
 }
