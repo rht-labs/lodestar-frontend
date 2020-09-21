c5292a15efdff7f62371f94ec6dcc72ffec48484
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-09-21 13:32:08 -0600

    fixed build errors

--- src/components/engagement_edit_modals/activity_history_details_modal.tsx
+++ src/components/engagement_edit_modals/activity_history_details_modal.tsx
@@ -5,7 +5,6 @@ import { EditModalTemplate } from '../../layout/edit_modal_template';
 import { GitCommit } from '../../schemas/git_commit';
 import { ActivityHistoryLineItem } from '../activity_history_line_item/activity_history_line_item';
 import { Engagement } from '../../schemas/engagement';
-import { FormManager } from '../../context/form_manager/form_manager';
 export interface ActivityHistoryDetailsModalProps {
   isOpen: boolean;
   engagement: Engagement;
