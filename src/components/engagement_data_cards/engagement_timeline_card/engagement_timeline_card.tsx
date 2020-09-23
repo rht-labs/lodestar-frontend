8c8eab0e100df4758b11961cb1fb6a8dc3796b9f
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-09-23 08:35:04 -0600

    removed unused import

--- src/components/engagement_data_cards/engagement_timeline_card/engagement_timeline_card.tsx
+++ src/components/engagement_data_cards/engagement_timeline_card/engagement_timeline_card.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { uuid } from 'uuidv4';
 import { DataCard } from '../data_card';
 import { Artifact } from '../../../schemas/engagement';
