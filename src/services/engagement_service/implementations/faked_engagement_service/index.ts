6d0eb60737e6f98ee56b3763aa7ccfabe5d4eaf3
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-09-24 22:05:53 -0600

    Engagement Artifacts (#328)
    * initial engagemetn artifacts
    
    * fixes for tests
    
    * add/edit artifact modal
    
    * removed unused import
    
    * tests
    
    * renamed guid to id
    
    * fixes for save
    
    * updated serializer. added 'other' option to artifact type
    
    * adapted column widths, added link to column title
    
    * updated snapshot test

--- src/services/engagement_service/implementations/faked_engagement_service/index.ts
+++ src/services/engagement_service/implementations/faked_engagement_service/index.ts
@@ -12,7 +12,7 @@ export class FakedEngagementService implements EngagementService {
   async createEngagement(data: Engagement): Promise<Engagement> {
     return data;
   }
-  async saveEngagement(data: any): Promise<Engagement> {
+  async saveEngagement(data: Engagement): Promise<Engagement> {
     return data as Engagement;
   }
   async launchEngagement(data: any): Promise<Engagement> {
