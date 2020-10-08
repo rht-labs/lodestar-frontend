a9025d4e955fcb929731bddf141b2497ccb1e444
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-10-08 15:37:26 -0600

    added initial test outline

---
+++ src/context/engagement_form_context/__tests__/engagement_form_context.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+describe('Engagement Form Context', () => {
+  test('saveChanges should call engagementContext.saveEngagement', () => {
+    expect(true).toBe(false);
+  });
+  test('can register a field group', () => {
+    expect(true).toBe(false);
+  });
+  test('can register a field', () => {
+    expect(true).toBe(false);
+  });
+  test('can update engagement fields', () => {
+    expect(true).toBe(false);
+  });
+  test('saving an engagement should clear the current changes', () => {
+    expect(true).toBe(false);
+  });
+  test('calling clearCurrentChanges will clear the current changes', () => {
+    expect(true).toBe(false);
+  });
+});
