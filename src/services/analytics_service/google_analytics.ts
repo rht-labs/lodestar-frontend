412d81df3853548a714b5150778e6a7544dd74ff
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-10-14 14:11:33 -0600

    Update google_analytics.ts

--- src/services/analytics_service/google_analytics.ts
+++ src/services/analytics_service/google_analytics.ts
@@ -6,13 +6,6 @@ export interface GoogleAnalyticsOptions {
   trackingCode: string;
 }
 
-/**
- * Service calls in this file should be wrapped in a try/catch.
- * Errors in reporting analytics data should not result in an error that
- * adversely affects the application by causing it to crash or show an error
- * to the user.
- */
-
 export class GoogleAnalytics implements AnalyticsService {
   constructor(options: GoogleAnalyticsOptions) {
     this.initialize(options.trackingCode);
