0c7e250d17628d8298ea1d44510d88a7dfe1061b
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-11-03 09:50:13 -0700

    added warning TODO

--- src/components/engagement_categories/engagement_editable_categories.tsx
+++ src/components/engagement_categories/engagement_editable_categories.tsx
@@ -10,13 +10,13 @@ import { Feature } from '../feature/feature';
 
 export function EngagementEditableCategories({
   engagementCategories,
-  onSave: propsOnSave,
   engagement,
 }: {
   engagementCategories?: EngagementCategory[];
   onSave: (engagement: Engagement) => void;
   engagement: Engagement;
 }) {
+  // TODO: This should come from props. This is a hotfix and should be replaced to use the save method that is passed from props.
   const { saveEngagement } = useEngagements();
   const [chips, setChips] = useState<string[]>([]);
   const [editMode, setEditMode] = useState(false);
