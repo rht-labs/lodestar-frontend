c01cc51d30a38617f852220e8284222435ff6e6a
Venus Izadi <mizadi@redhat.com>
2020-11-17 09:46:41 -0500

    replace constant with variable

--- src/routes/scroll_to_top.tsx
+++ src/routes/scroll_to_top.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { APPLICATION_CONTENT_CONTAINER_ID } from '../common/constants'
 
 export default function ScrollToTop({children}) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    document.getElementById('main-application-content')?.scrollTo?.(0, 0);
+    document.getElementById(APPLICATION_CONTENT_CONTAINER_ID)?.scrollTo?.(0, 0);
   }, [pathname]);
 
   return children;
