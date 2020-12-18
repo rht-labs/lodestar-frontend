a4839ca75db14517fc6431c88bd4c544711366f3
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-12-18 13:08:39 -0700

    fixes for tests

--- src/components/engagement_data_cards/__tests__/data_cards.spec.tsx
+++ src/components/engagement_data_cards/__tests__/data_cards.spec.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Engagement } from '../../../schemas/engagement';
 import { EngagementSummaryCard } from '../engagement_summary_card/engagement_summary_card';
-import { OpenShiftClusterSummaryCard } from '../openshift_cluster_card/openshift_cluster_card';
+import { HostingEnvironmentCard } from '../hosting_environment_card/hosting_environment_card';
 import { PointOfContactCard } from '../point_of_contact_card/point_of_contact_card';
 import { TestStateWrapper } from '../../../common/test_state_wrapper';
-import {APP_FEATURES} from "../../../common/app_features";
-import {FeatureToggles} from "../../../context/feature_context/feature_toggles";
-import { DataCard } from "../data_card";
+import { APP_FEATURES } from '../../../common/app_features';
+import { FeatureToggles } from '../../../context/feature_context/feature_toggles';
+import { DataCard } from '../data_card';
 import { MemoryRouter } from 'react-router';
 
 describe('Engagement summary card', () => {
@@ -15,15 +15,15 @@ describe('Engagement summary card', () => {
     expect(
       render(
         <MemoryRouter>
-        <TestStateWrapper>
-          <EngagementSummaryCard
-            onSave={() => {}}
-            onChange={() => {}}
-            currentEngagement={Engagement.fromFake(true)}
-            currentEngagementChanges={Engagement.fromFake(true)}
-            missingRequiredFields={[]}
-          />
-        </TestStateWrapper>
+          <TestStateWrapper>
+            <EngagementSummaryCard
+              onSave={() => {}}
+              onChange={() => {}}
+              currentEngagement={Engagement.fromFake(true)}
+              currentEngagementChanges={Engagement.fromFake(true)}
+              missingRequiredFields={[]}
+            />
+          </TestStateWrapper>
         </MemoryRouter>
       )
     ).toMatchSnapshot();
@@ -35,7 +35,7 @@ describe('OpenShift Cluster Summary', () => {
     expect(
       render(
         <TestStateWrapper>
-          <OpenShiftClusterSummaryCard
+          <HostingEnvironmentCard
             onSave={() => {}}
             onChange={() => {}}
             currentEngagement={Engagement.fromFake(true)}
@@ -67,21 +67,21 @@ describe('Point of Contact Card', () => {
 });
 
 describe('Edit button', () => {
-
   test('should not be rendered for reader role', async () => {
     const Component = () => (
       <MemoryRouter>
         <FeatureToggles features={[APP_FEATURES.reader]}>
-          <DataCard title={'test'}
-                    children={<></>}
-                    actionButton={() => <div />}
-                    trailingIcon={() => <div />}/>
+          <DataCard
+            title={'test'}
+            children={<></>}
+            actionButton={() => <div />}
+            trailingIcon={() => <div />}
+          />
         </FeatureToggles>
       </MemoryRouter>
     );
 
     const wrapper = render(<Component />);
-    expect(wrapper.findByTestId('data-card-button')).toMatchObject({})
+    expect(wrapper.findByTestId('data-card-button')).toMatchObject({});
   });
-
 });
