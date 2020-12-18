71e015c0aed1c518cc38107947859aef435070df
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-12-18 15:51:12 -0700

    fixed tests, changed rht-labs to example.com

--- src/components/engagement_form_fields/subdomain.tsx
+++ src/components/engagement_form_fields/subdomain.tsx
@@ -57,7 +57,7 @@ export function SubdomainFormField({
         <div>
           Applications will live at:&nbsp;
           <strong>{`${getSubdomainHelperText()}`}</strong>
-          <span style={{ fontStyle: 'italic' }}>{`.region.rht-labs.com`}</span>
+          <span style={{ fontStyle: 'italic' }}>{`.region.example.com`}</span>
           &nbsp;&nbsp;
           <Tooltip content="The full domain is shown as an example. The actual domain(s) used within the environment(s) will be available as part of the status once the engagement is launched">
             <InfoCircleIcon></InfoCircleIcon>
