235637de72cb983081d94a4b936ae1aba3a51147
Venus Izadi <mizadi@redhat.com>
2021-01-13 16:05:40 -0500

    update the tooltip

--- src/components/engagement_edit_modals/user_row.tsx
+++ src/components/engagement_edit_modals/user_row.tsx
@@ -160,7 +160,7 @@ export const UserRow = ({
         <Feature name={APP_FEATURES.writer}>
           <GridItem span={1} style={{ paddingTop: '1rem', paddingLeft: '1.5rem' }}>
             <Tooltip
-              content={"Select users you want to reset (Active Engagements only)"}
+              content={"This feature will be available soon"}
               entryDelay={0}
               exitDelay={10}
               isContentLeftAligned={true}
