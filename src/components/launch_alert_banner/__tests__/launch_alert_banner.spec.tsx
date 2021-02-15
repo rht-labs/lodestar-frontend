5eed9ab65581b3dd85d9fada624c7b6da105b55f
Venus Izadi <mizadi@redhat.com>
2021-02-15 12:03:57 -0500

    add tests - make the delete modal name unique

--- src/components/launch_alert_banner/__tests__/launch_alert_banner.spec.tsx
+++ src/components/launch_alert_banner/__tests__/launch_alert_banner.spec.tsx
@@ -1,22 +1,58 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render, RenderResult } from '@testing-library/react';
 import { LaunchAlertBanner } from '../launch_alert_banner';
 import { Engagement } from '../../../schemas/engagement';
 import { MemoryRouter } from 'react-router';
-describe('Engagement Navigation ', () => {
-  test('matches the snapshot', () => {
-    expect(
-      render(
-        <MemoryRouter>
+import {FeatureToggles} from "../../../context/feature_context/feature_toggles";
+
+describe('Engagement launch alert banner ', () => {
+  const getLaunchBar = () =>
+    render(
+      <MemoryRouter>
+        <FeatureToggles features={['reader', 'writer']}>
           <LaunchAlertBanner
-            isLaunchable
+            isLaunchable={true}
+            engagement={Engagement.fromFake(true)}
+            missingRequiredFields={[]}
+            onLaunch={() => {}}
+            requiredFields={[]}
+          />
+        </FeatureToggles>
+      </MemoryRouter>
+    );
+
+  test('matches the snapshot',  () => {
+    expect(
+      getLaunchBar
+    ).toMatchSnapshot();
+  });
+
+  test('shows the delete button for launchable engagements', async () => {
+    let view: RenderResult;
+    await act(async () => {
+      view = getLaunchBar();
+    });
+    expect(await view.findByTestId('delete-button')).toBeDefined();
+  });
+
+  test('do not show the delete button for non-launchable engagements', async () => {
+    const getLaunchBarForNonLaunchableEngagement = () =>
+      render(
+      <MemoryRouter>
+          <LaunchAlertBanner
+            isLaunchable={false}
             engagement={Engagement.fromFake(true)}
             missingRequiredFields={[]}
             onLaunch={() => {}}
             requiredFields={[]}
           />
         </MemoryRouter>
-      )
-    ).toMatchSnapshot();
+      );
+
+    let view: RenderResult;
+    await act(async () => {
+      view = getLaunchBarForNonLaunchableEngagement();
+    });
+    expect(await view.queryByTestId('delete-button')).toBeNull();
   });
 });
