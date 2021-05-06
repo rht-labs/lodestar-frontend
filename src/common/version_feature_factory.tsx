7cf55de29b6c00e0da29a628d74540fba765b00e
Lucas K Allmon <lallmon@redhat.com>
2021-05-06 08:55:56 -0700

    fix(flag): target v1.2.0 for dashboards (#476)

--- src/common/version_feature_factory.tsx
+++ src/common/version_feature_factory.tsx
@@ -7,7 +7,7 @@ export type FeatureVersionMap = {
 
 export const FEATURE_VERSION_MAP: FeatureVersionMap = {
   resetUser: 'v1.1.50',
-  newDashboard: 'v0.0.1',
+  newDashboard: 'v1.2.0',
 };
 const getSemverFromVersionString = (version: string = ''): string => {
   return semver.clean(version);
