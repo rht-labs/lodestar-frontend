fc1d9d846156d81f545208b67418dfd5543e19e6
Spencer Stolworthy <sdstolworthy@gmail.com>
2021-06-10 16:39:01 -0600

    fix for failed tests

--- src/components/dashboard/widgets/dw_last_demo.spec.tsx
+++ src/components/dashboard/widgets/dw_last_demo.spec.tsx
@@ -1,17 +1,22 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import { mockEngagementArtifact } from '../../../mocks/engagement_mocks';
 import { DwLastDemo } from './dw_last_demo';
 
 describe('Dashboard last demos', () => {
   test('has the correct title', () => {
-    const component = render(<DwLastDemo demos={[]} />);
+    const component = render(<DwLastDemo demos={[]} engagements={[]} />);
     expect(component.getByText('Last 5 Demos')).toBeDefined();
   });
   test('shows the demos', () => {
     const artifacts = new Array(10).fill(null).map(mockEngagementArtifact);
     const demos = artifacts.map(d => ({ ...d, type: 'demo' }));
-    const component = render(<DwLastDemo demos={demos} />);
+    const component = render(
+      <MemoryRouter>
+        <DwLastDemo demos={demos} engagements={[]} />
+      </MemoryRouter>
+    );
     for (let demo of demos) {
       expect(component.getByText(demo.description)).toBeDefined();
     }
