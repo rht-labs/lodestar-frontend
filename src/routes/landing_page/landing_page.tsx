643427e181c1e68bab133a7199988ceb4b26a77d
Lucas K Allmon <lucaskallmon@gmail.com>
2021-07-01 08:55:29 -0700

    chore (videos): add

--- src/routes/landing_page/landing_page.tsx
+++ src/routes/landing_page/landing_page.tsx
@@ -24,10 +24,77 @@ export const LandingPage = () => {
   return (
     <div className="landing-page">
       <Page header={pageHeader}>
-        <PageSection style={{ padding: 0 }}>
-          <div style={{ height: 25 }}></div>
-        </PageSection>
-        <div className="jumbotron-container">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div className="jumbotron-container" style={{ height: '100vh' }}>
+          <div className="video-container">
+            <div className="aspect-ratio--16x9 homepage-video">
+              <iframe
+                className="aspect-ratio-object"
+                id="kaltura_player"
+                src="https://cdnapisec.kaltura.com/p/2300461/sp/230046100/embedIframeJs/uiconf_id/42569541/partner_id/2300461?iframeembed=true&playerId=kaltura_player&entry_id=1_e1kbegt0&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_aphcva5b"
+                width="768"
+                height="432"
+                allowFullScreen
+                allow="autoplay *; fullscreen *; encrypted-media *"
+                sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
+                frameBorder="0"
+                title="Kaltura Player"
+              ></iframe>
+            </div>
+            <div className="aspect-ratio--16x9 homepage-video">
+              <iframe
+                className="aspect-ratio-object"
+                id="kaltura_player"
+                src="https://cdnapisec.kaltura.com/p/2300461/sp/230046100/embedIframeJs/uiconf_id/42569541/partner_id/2300461?iframeembed=true&playerId=kaltura_player&entry_id=1_e1kbegt0&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_aphcva5b"
+                width="768"
+                height="432"
+                allowFullScreen
+                allow="autoplay *; fullscreen *; encrypted-media *"
+                sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
+                frameBorder="0"
+                title="Kaltura Player"
+              ></iframe>
+            </div>
+            <div className="aspect-ratio--16x9 homepage-video">
+              <iframe
+                className="aspect-ratio-object"
+                id="kaltura_player"
+                src="https://cdnapisec.kaltura.com/p/2300461/sp/230046100/embedIframeJs/uiconf_id/42569541/partner_id/2300461?iframeembed=true&playerId=kaltura_player&entry_id=1_e1kbegt0&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_aphcva5b"
+                width="768"
+                height="432"
+                allowFullScreen
+                allow="autoplay *; fullscreen *; encrypted-media *"
+                sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
+                frameBorder="0"
+                title="Kaltura Player"
+              ></iframe>
+            </div>
+          </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
           <div className="cta">
             <span>LodeStar</span>
             <Link to="/app">
@@ -36,8 +103,6 @@ export const LandingPage = () => {
           </div>
           <div className="background-image" />
           <div className="background-image-overlay" />
-        </div>
-        <PageSection></PageSection>
       </Page>
     </div>
   );
