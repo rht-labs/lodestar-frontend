1791adae03b32fc9a1419cce8bb1213840add989
Philip Double <philip.double@gmail.com>
2021-07-21 17:09:24 -0400

    updated fake data to have unique description

--- src/schemas/config.ts
+++ src/schemas/config.ts
@@ -54,7 +54,7 @@ export abstract class Config {
         },
         {
           url: "https://cdnapisec.kaltura.com/p/2300461/sp/230046100/embedIframeJs/uiconf_id/42569541/partner_id/2300461?iframeembed=true&playerId=kaltura_player&entry_id=1_e1kbegt0&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_aphcva5b",
-          description: "this is a video"
+          description: "ls video 2"
         }
       ],
       allowVersionOverride: true,
