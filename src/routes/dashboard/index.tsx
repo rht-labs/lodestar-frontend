9125c5b324dc44a73340cbb33791f7525c5c5eae
Philip Double <philip.double@gmail.com>
2021-10-04 11:43:38 -0400

    removing practice count from test

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -23,12 +23,13 @@ import { DwLastDemo } from '../../components/dashboard/widgets/dw_last_demo';
 import { DwLastUpdated } from '../../components/dashboard/widgets/dw_last_updated_engagements';
 import { DwLastUseCases } from '../../components/dashboard/widgets/dw_last_use_cases';
 import { DwLastWeeklyReport } from '../../components/dashboard/widgets/dw_last_weekly_report';
-import { DwTopPractices } from '../../components/dashboard/widgets/dw_top_practices';
+//import { DwLastWeeklyReport2 } from '../../components/dashboard/widgets/dw_last_weekly_report2';
+//import { DwTopPractices } from '../../components/dashboard/widgets/dw_top_practices';
 import { DwTopTags } from '../../components/dashboard/widgets/dw_top_tags';
 import { EnabledUsersFilter } from '../../services/enabled_users_service/enabled_users_service';
 import { EngagementQueryMediator } from '../../components/dashboard/widgets/engagement_query_mediator';
 import { Feature } from '../../components/feature/feature';
-import { PracticeCountFilter } from '../../services/practice_count_service/practice_count_service';
+//import { PracticeCountFilter } from '../../services/practice_count_service/practice_count_service';
 import { SortOrder } from '../../services/engagement_service/engagement_service';
 import { SummaryCountFilter } from '../../services/summary_count_service/summary_count_service';
 import { createBase64ParseableFilter } from '../engagement_list/engagement_list_route';
@@ -37,7 +38,7 @@ import { useEnabledUsers } from '../../hooks/use_enabled_users';
 import { useEngagementCollection } from '../../hooks/engagement_collection_hook';
 import { useEngagementFormConfig } from '../../context/engagement_config_context/engagement_config_hook';
 import { useHistory } from 'react-router';
-import { usePracticeCount } from '../../hooks/use_practice_count';
+//import { usePracticeCount } from '../../hooks/use_practice_count';
 import { useServiceProviders } from '../../context/service_provider_context/service_provider_context';
 import { useSummaryCount } from '../../hooks/use_summary_count';
 import { useVersion } from '../../context/version_context/version_context';
@@ -70,7 +71,7 @@ export function Dashboard() {
     artifactService,
     useCaseService,
     enabledUsersService,
-    practiceCountService,
+ //   practiceCountService,
     summaryCountService,
   } = useServiceProviders();
   const [isRegionSelectOpen, setIsRegionSelectOpen] = useState(false);
@@ -89,16 +90,16 @@ export function Dashboard() {
     undefined
   );
 
-  const practiceFetcher = useCallback(() => {
-    const filter: PracticeCountFilter = {
-      regions: selectedRegions,
-    };
-    return practiceCountService.getPracticeCount(filter);
-  }, [practiceCountService, selectedRegions]);
+  // const practiceFetcher = useCallback(() => {
+  //   const filter: PracticeCountFilter = {
+  //     regions: selectedRegions,
+  //   };
+  //   return practiceCountService.getPracticeCount(filter);
+  // }, [practiceCountService, selectedRegions]);
 
-  const { practiceCount, isLoading: isLoadingPractices } = usePracticeCount(
-    practiceFetcher
-  );
+  // const { practiceCount, isLoading: isLoadingPractices } = usePracticeCount(
+  //   practiceFetcher
+  // );
 
   const categoryFetcher = useCallback(() => {
     const filter: CategoryFilter = {
@@ -239,18 +240,36 @@ export function Dashboard() {
                   }}
                 />
               </GridItem>
+              {/* <GridItem sm={12} xl={12} xl2={6}>
+                {withArtifacts(
+                  DwLastWeeklyReport2,
+                  () =>
+                    artifactService.getArtifacts({
+                      page: 1,
+                      perPage: 5,
+                      type: 'weeklyReport',
+                      startDate: dateFilter?.startDate,
+                      endDate: dateFilter?.endDate,
+                      regions: selectedRegions,
+                      sortOrder: 'DESC',
+                      sortFields: 'updated',
+                    }),
+
+                  engagementService.getEngagementById
+                )}
+              </GridItem> */}
               <GridItem colSpan={1} sm={12} md={6} xl={6} xl2={6}>
                 <DashboardPeopleEnabledCard
                   usersEnabled={enabledUsers}
                   isLoading={isLoadingEnabledUsers}
                 />
               </GridItem>
-              <GridItem colSpan={1} sm={12} md={6} xl={6} xl2={6}>
+              {/* <GridItem colSpan={1} sm={12} md={6} xl={6} xl2={6}>
                 <DwTopPractices
                   practices={practiceCount}
                   isLoading={isLoadingPractices}
                 />
-              </GridItem>
+              </GridItem> */}
               <GridItem colSpan={1} sm={12} md={6} xl={6} xl2={6}>
                 <DwTopTags
                   categories={categories}
