29898dd0d886f7ebd5ed0b983b571f43c2955ef1
Jacob See <jacob@Jacobs-MBP.localdomain>
2022-02-17 16:07:09 -0800

    Attempt to fix routing error in artifact component spec

--- src/components/dashboard/widgets/dw_last_artifact.spec.tsx
+++ src/components/dashboard/widgets/dw_last_artifact.spec.tsx
@@ -1,17 +1,27 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
 import { mockEngagementUseCase } from '../../../mocks/engagement_mocks';
 import { DwLastArtifacts } from './dw_last_artifact';
 import { DwLastUseCases } from './dw_last_use_cases';
 
 describe('Last Use Cases dashboard widget', () => {
   test('has the correct title', () => {
-    const component = render(<DwLastArtifacts artifacts={[]} />);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastArtifacts artifacts={[]} />
+      </Router>
+    );
     expect(component.getByText('Artifacts')).toBeDefined();
   });
   test('shows the artifacts', () => {
     const artifacts = new Array(10).fill(null).map(mockEngagementUseCase);
-    const component = render(<DwLastUseCases useCases={artifacts} />);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastArtifacts artifacts={[]} />
+      </Router>
+    );
     for (let artifact of artifacts) {
       expect(component.getByText(artifact.description)).toBeDefined();
     }
