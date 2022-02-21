bfec8904426c1c5a2dce7f2ff052077fe973dbb5
Jacob See <jacob.d.see@gmail.com>
2022-02-21 10:51:02 -0800

    back to work in the unit test mines

--- src/components/dashboard/widgets/dw_last_artifact.spec.tsx
+++ src/components/dashboard/widgets/dw_last_artifact.spec.tsx
@@ -1,19 +1,29 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { mockEngagementUseCase } from '../../../mocks/engagement_mocks';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { mockEngagementArtifact } from '../../../mocks/engagement_mocks';
 import { DwLastArtifacts } from './dw_last_artifact';
-import { DwLastUseCases } from './dw_last_use_cases';
 
 describe('Last Use Cases dashboard widget', () => {
   test('has the correct title', () => {
-    const component = render(<DwLastArtifacts artifacts={[]} />);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastArtifacts artifacts={[]} />
+      </Router>
+    );
     expect(component.getByText('Artifacts')).toBeDefined();
   });
   test('shows the artifacts', () => {
-    const artifacts = new Array(10).fill(null).map(mockEngagementUseCase);
-    const component = render(<DwLastUseCases useCases={artifacts} />);
+    const artifacts = new Array(10).fill(null).map(mockEngagementArtifact);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastArtifacts artifacts={artifacts} />
+      </Router>
+    );
     for (let artifact of artifacts) {
-      expect(component.getByText(artifact.description)).toBeDefined();
+      expect(component.getByText(artifact.description.substring(0, artifact.description.lastIndexOf(" ")))).toBeDefined();
+      // The last word will be wrapped in a span with an icon, so only match until the last space
     }
   });
 });
--- src/components/dashboard/widgets/dw_last_use_cases.spec.tsx
+++ src/components/dashboard/widgets/dw_last_use_cases.spec.tsx
@@ -1,17 +1,27 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
 import { mockEngagementUseCase } from '../../../mocks/engagement_mocks';
 import { DwLastUseCases } from './dw_last_use_cases';
 
 
 describe('Last Use Cases dashboard widget', () => {
   test('has the correct title', () => {
-    const component = render(<DwLastUseCases useCases={[]} />);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastUseCases useCases={[]} />
+      </Router>
+    );
     expect(component.getByText('Use Cases')).toBeDefined();
   });
   test('shows the use cases', () => {
     const useCases = new Array(10).fill(null).map(mockEngagementUseCase);
-    const component = render(<DwLastUseCases useCases={useCases} />);
+    const component = render(
+      <Router history={createMemoryHistory({})}>
+        <DwLastUseCases useCases={useCases} />
+      </Router>
+    );
     for (let useCase of useCases) {
       expect(component.getByText(useCase.description)).toBeDefined();
     }
