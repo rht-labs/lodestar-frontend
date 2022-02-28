813c8ce903641244d7315835de8b0b6632526f55
Jacob See <jacob.d.see@gmail.com>
2022-02-27 17:11:34 -0800

    Artifact card should use title rather than description. Description becomes a mouseover. Trim spaces when adding external link icon for text wrapping purposes.

--- src/components/link_or_span/link_or_span.tsx
+++ src/components/link_or_span/link_or_span.tsx
@@ -3,18 +3,19 @@ import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 export interface LinkOrSpanProps {
   href?: string;
+  tooltip?: string;
   children: any;
 }
 
 const addLinkIconWithWrapCorrection = (children: any) => {
-  const childrenArray = String(children).split(' ');
+  const childrenArray = String(children).trim().split(' ');
   return <>{childrenArray.slice(0, -1).join(' ')} <span className="nowrap">{childrenArray.slice(-1)}&nbsp;<ExternalLinkAltIcon className="externalAltLinkIcon"/></span></>;
 }
 
-export const LinkOrSpan = ({ href, children }: LinkOrSpanProps) => {
+export const LinkOrSpan = ({ href, children, tooltip }: LinkOrSpanProps) => {
   if (!!href) {
-    return <a href={href} target="_blank" rel="noreferrer">{addLinkIconWithWrapCorrection(children)}</a>;
+    return <a href={href} title={tooltip} target="_blank" rel="noreferrer">{addLinkIconWithWrapCorrection(children)}</a>;
   } else {
-    return <span>{children}</span>;
+    return <span title={tooltip}>{children}</span>;
   }
 };
